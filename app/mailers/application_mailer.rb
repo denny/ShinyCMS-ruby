db73ba518f6ba088794fad265d2dabb497859cf9
Denny de la Haye <2018@denny.me>
2019-09-26 23:24:45 +0100

    Rubocop config and some rubocop fixes

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,4 @@
+# Application Mailer
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,4 @@
+# Application Record
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
