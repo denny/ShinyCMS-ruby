80dc14d25171048970c66f3873fac93ede82b81f
Denny de la Haye <2019@denny.me>
2019-09-29 10:09:12 +0100

    Fix rubocop fails, update naive test strings, remove some optional values

--- spec/factories/page_sections.rb
+++ spec/factories/page_sections.rb
@@ -1,12 +1,8 @@
 FactoryBot.define do
   factory :page_section do
-    name { "MyString" }
-    description { "MyText" }
-    title { "MyString" }
-    slug { "MyString" }
-    section_id { 1 }
-    sort_order { 1 }
+    name   { 'Test Section' }
+    title  { 'Test Section' }
+    slug   { 'test_section' }
     hidden { false }
-    last_published_at { "2019-09-28 10:13:01" }
   end
 end
--- spec/factories/pages.rb
+++ spec/factories/pages.rb
@@ -1,13 +1,10 @@
 FactoryBot.define do
   factory :page do
-    name { "MyString" }
-    description { "MyText" }
-    title { "MyString" }
-    slug { "MyString" }
-    template_id { 1 }
-    section_id { 1 }
-    sort_order { 1 }
+    name   { 'Test Page' }
+    title  { 'Test Page' }
+    slug   { 'test_page' }
     hidden { false }
-    last_published_at { "2019-09-28 10:01:53" }
+    template_id { 1 }
+    hidden { false }
   end
 end
