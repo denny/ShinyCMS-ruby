4e855d0ba6ede0a697a5397bfccdedba943e5034
Denny de la Haye <2018@denny.me>
2019-09-30 22:45:55 +0100

    Add filename to template table, remove slug and title

---
+++ db/migrate/20190930213046_fix_template_columns.rb
@@ -0,0 +1,11 @@
+class FixTemplateColumns < ActiveRecord::Migration[6.0]
+  def change
+    add_column :page_templates, :filename, :integer
+
+    PageTemplate.update_all( filename: 'NOT-NULL' )
+    change_column_null :page_templates, :filename, false
+
+    remove_column :page_templates, :title, :string
+    remove_column :page_templates, :slug,  :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_29_210346) do
+ActiveRecord::Schema.define(version: 2019_09_30_213046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,8 +32,7 @@ ActiveRecord::Schema.define(version: 2019_09_29_210346) do
   create_table "page_templates", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
-    t.string "title", null: false
-    t.string "slug", null: false
+    t.integer "filename", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
