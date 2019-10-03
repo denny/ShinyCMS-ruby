575d5f1c2da54ecb017575f0e6a553af37d68cd3
Denny de la Haye <2019@denny.me>
2019-10-03 18:04:44 +0100

    Add slightly broken test to test new pre-push git hook

--- spec/request/admin/pages_spec.rb
+++ spec/request/admin/pages_spec.rb
@@ -5,6 +5,15 @@ RSpec.describe 'Admin: Pages', type: :request do
     it 'fetches the list of pages in the admin area' do
       get '/admin/pages'
       expect( response ).to have_http_status :ok
+      expect( response.body ).to include 'List pages'
+    end
+  end
+
+  describe 'GET /admin/page/add' do
+    it 'loads the form to add a new page' do
+      get '/admin/page/add'
+      expect( response ).to have_http_status :ok
+      expect( response.body ).to include 'Add a new page'
     end
   end
 end
