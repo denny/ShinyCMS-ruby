23e2b1e6e0dde4bb1825adba6c1ea4a83a35a73b
Denny de la Haye <2019@denny.me>
2019-10-03 01:24:13 +0100

    Move the admin templates and assets to more clearly separated locations, to make them easier to upgrade (replace) wholesale in future

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w[ admin-area.css main-site.css ]
+Rails.application.config.assets.precompile += %w[
+  main-site.css
+  admin/admin-area.css
+]
