ef994d7ab7bbdebd1dff41f40035f0f6aa7e3805
Denny de la Haye <denny.d@38degrees.org.uk>
2019-10-04 22:41:19 +0100

    Minor fidding with layout, no functional changes

--- app/models/page_section.rb
+++ app/models/page_section.rb
@@ -1,26 +1,29 @@
 # Model for page sections
 class PageSection < ApplicationRecord
-  # Associations
-  has_many :pages,
-           -> { where( hidden: false ).order( :sort_order ) },
-           foreign_key: 'section_id', inverse_of: 'section'
-  has_many :hidden_pages,
-           -> { where( hidden: true ).order( :sort_order ) },
-           foreign_key: 'section_id', class_name: 'Page', inverse_of: 'section'
+  has_many  :pages,
+            -> { where( hidden: false ).order( :sort_order ) },
+            foreign_key: 'section_id', inverse_of: 'section'
 
-  belongs_to :section,
-             class_name: 'PageSection', optional: true, inverse_of: 'sections'
-  has_many   :sections,
-             -> { where( hidden: false ).order( :sort_order ) },
-             class_name: 'PageSection',
-             foreign_key: 'section_id', inverse_of: 'section'
+  has_many  :hidden_pages,
+            -> { where( hidden: true ).order( :sort_order ) },
+            class_name: 'Page',
+            foreign_key: 'section_id', inverse_of: 'section'
+
+  has_many  :sections,
+            -> { where( hidden: false ).order( :sort_order ) },
+            class_name: 'PageSection',
+            foreign_key: 'section_id', inverse_of: 'section'
+
+  belongs_to  :section,
+              class_name: 'PageSection',
+              optional: true, inverse_of: 'sections'
 
   # Instance methods
 
   # Return the default page for this section if one is set
   # If the default isn't set, return the first page in this section
   def default_page
-    return pages.find( self[:default_page_id] ) if self[:default_page_id]
+    return pages.find self[ :default_page_id ] if self[ :default_page_id ]
 
     pages.first
   end
