fbdaf332c793a0639bcc6b127e5135c4e31c42ce
Denny de la Haye <2019@denny.me>
2019-10-05 00:16:01 +0100

    Setting name cannot be null

--- db/migrate/20191004223601_create_site_settings.rb
+++ db/migrate/20191004223601_create_site_settings.rb
@@ -1,7 +1,7 @@
 class CreateSiteSettings < ActiveRecord::Migration[6.0]
   def change
     create_table :site_settings do |t|
-      t.string :name
+      t.string :name, :not_null
       t.string :value
 
       t.timestamps
