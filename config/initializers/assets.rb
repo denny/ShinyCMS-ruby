9b7df5263fe146228da7959cefc6fc73545aa5e9
Denny de la Haye <2019@denny.me>
2019-10-04 22:35:28 +0100

    Also needed for SCSS support, oops

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,5 +14,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w[
   main-site.css
-  admin/admin-area.css
+  admin/admin-area.scss
 ]
