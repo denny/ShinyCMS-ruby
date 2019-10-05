15572a6d8685a3ea1d23ab60e160ab5865982da6
Denny de la Haye <2018@denny.me>
2019-10-05 11:20:51 +0100

    Flesh out Page.default_page method

--- app/models/page.rb
+++ app/models/page.rb
@@ -20,15 +20,23 @@ class Page < ApplicationRecord
     Page.exists?( hidden: true )
   end
 
-  # TODO: Return the default top-level page
+  # Return the default top-level page
   def self.default_page
-    # 1.  Page.find( Config.default_page )
-    # 2.  Section.find( Config.default_section )
-    # 2a. default_section.pages.default_page
-    # 2b. default_section.pages.first
+    page = find_default_page
+    return page if page
+
+    section = PageSection.default_section
+    page    = section&.pages&.default_page
+    page  ||= section&.pages&.first
+    return page if page
 
     return Page.top_level_pages.first if Page.top_level_pages.first
 
     Page.first
+  end
+
+  def self.find_default_page
+    name = Setting.get 'default_page'
+    Page.where( name: name ).or( Page.where( slug: name ) ).first
   end
 end
