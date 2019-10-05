93be1622dfbf397009bce1c7d6e72671f6324bdc
Denny de la Haye <2018@denny.me>
2019-10-05 09:20:34 +0100

    Site settings page in admin area working for add/delete

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,13 +1,40 @@
 # Admin controller for CMS settings
-class Admin::SettingsController < ApplicationController
-  # Giant settings form
+class Admin::SettingsController < AdminController
+  # Render site settings form
   def index
-    @settings = SiteSettings.all.sort_by( :name )
+    @settings = Setting.order( :name )
   end
 
-  # Form submitted; update any changed settings
+  # New setting form submitted
+  def create
+    @setting = Setting.new( setting_params )
+
+    if @setting.save
+      flash[ :notice ] = 'New setting added'
+    else
+      flash[ :alert ] = 'Failed to create new setting'
+    end
+    redirect_to admin_settings_path
+  end
+
+  # Main form submitted; update any changed settings
   def update
     flash[ :status ] = 'Settings updated'
-    render :index
+    redirect_to admin_settings_path
+  end
+
+  def delete
+    if Setting.delete( params[ :id ] )
+      flash[ :notice ] = 'Setting deleted'
+    else
+      flash[ :alert ] = 'Failed to delete setting'
+    end
+    redirect_to admin_settings_path
+  end
+
+  private
+
+  def setting_params
+    params.require( :setting ).permit( :name, :value )
   end
 end
