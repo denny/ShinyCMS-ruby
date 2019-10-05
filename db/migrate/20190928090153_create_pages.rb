71d0f49b39b5c9fbdf7bff1ebb9e0fa7099daa74
Denny de la Haye <2018@denny.me>
2019-10-05 01:49:34 +0100

    Poking migrations with a stick so they stop scrambling my nice tidy database

--- db/migrate/20190928090153_create_pages.rb
+++ db/migrate/20190928090153_create_pages.rb
@@ -2,14 +2,14 @@
 class CreatePages < ActiveRecord::Migration[6.0]
   def change
     create_table :pages do |t|
-      t.string :name
+      t.string :name, null: false
       t.text :description
-      t.string :title
-      t.string :slug
-      t.integer :template_id
+      t.string :title, null: false
+      t.string :slug, null: false
+      t.integer :template_id, null: false
       t.integer :section_id
       t.integer :sort_order
-      t.boolean :hidden
+      t.boolean :hidden, null: false, default: false
       t.datetime :last_published_at
 
       t.timestamps
