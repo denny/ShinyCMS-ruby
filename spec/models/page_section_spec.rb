27bcca123336cf43eb5d4975cee38b5da309cd16
Denny de la Haye <2019@denny.me>
2019-10-08 02:41:30 +0100

    Add test for finding explicitly-set default page within section

--- spec/models/page_section_spec.rb
+++ spec/models/page_section_spec.rb
@@ -2,21 +2,29 @@ require 'rails_helper'
 
 RSpec.describe PageSection, type: :model do
   context '.default_page' do
-    it 'returns the correct page' do
+    it 'returns the first page in the section if not default is set' do
       create :top_level_page
       section = create :page_section
       page001 = create :page, section: section
       page002 = create :page, section: section
       expect( page002.section.default_page ).to eq page001
     end
+    it 'returns the correct default page if one is set' do
+      create :top_level_page
+      section = create :page_section
+      page001 = create :page, section: section
+      page002 = create :page, section: section
+      section.update! default_page_id: page002.id
+      expect( page001.section.default_page ).to eq page002
+    end
   end
 
   context 'PageSection.default_section' do
     it 'returns the correct section' do
       create :page_section
-      section2 = create :page_section, name: 'Test2'
+      section2 = create :page_section, slug: 'default'
 
-      Setting.create! name: 'default_section', value: 'Test2'
+      Setting.create! name: 'Default section', value: 'default'
 
       expect( PageSection.default_section ).to eq section2
     end
