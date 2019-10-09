b69f0c047d42381798d63186129477fdff05e180
Denny de la Haye <2019@denny.me>
2019-10-09 12:43:32 +0100

    How many pucks would a pluck pick pluck if a pluck pick could pluck pucks?

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -4,6 +4,6 @@ class Setting < ApplicationRecord
 
   # Return the value of the specified setting
   def self.get( name )
-    where( name: name).pluck( :value ).first
+    where( name: name).pick( :value )
   end
 end
