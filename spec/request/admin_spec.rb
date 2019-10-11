670161c97036ebf7e50f0ee2af92d23d82db0ee7
Denny de la Haye <2018@denny.me>
2019-10-11 23:21:35 +0100

    Test IP whitelist

--- spec/request/admin_spec.rb
+++ spec/request/admin_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe 'Admin', type: :request do
       get '/admin'
       expect( response ).to have_http_status :found
     end
+    it 'still works with an IP whitelist set' do
+      Setting.find_or_create_by!( name: I18n.t( 'admin_ip_whitelist' ) )
+             .update!( value: '127.0.0.1' )
+      get '/admin'
+      expect( response ).to have_http_status :found
+    end
   end
 end
