1c7f71fb3e035fd7fe505a4e4a5c67942bbbd73b
Denny de la Haye <2018@denny.me>
2019-10-12 18:32:13 +0100

    Add test for deleting a site setting

--- spec/request/admin/settings_spec.rb
+++ spec/request/admin/settings_spec.rb
@@ -45,6 +45,25 @@ RSpec.describe 'Admin: Site Settings', type: :request do
     end
   end
 
+  describe 'DELETE /admin/setting/delete/:id' do
+    it 'deletes the specified setting' do
+      s1 = create :setting
+      s2 = create :setting, name: 'DO NOT WANT'
+      s3 = create :setting
+
+      delete admin_setting_delete_path( s2.id )
+
+      expect( response      ).to     have_http_status :found
+      follow_redirect!
+      expect( response      ).to     have_http_status :ok
+      expect( response.body ).to     include 'Site Settings'
+      expect( response.body ).to     include I18n.t 'setting_deleted'
+      expect( response.body ).to     include s1.name
+      expect( response.body ).to     include s3.name
+      expect( response.body ).not_to include s2.name
+    end
+  end
+
   describe 'POST /admin/settings' do
     it 'updates any settings that were changed' do
       create :setting, name: 'test_setting_one'
