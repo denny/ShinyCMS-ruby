41e5a3357646ee03fb92ec21d5479fbf53f417a5
Denny de la Haye <2018@denny.me>
2019-10-12 19:02:15 +0100

    Use path helpers, and line things up more neatly

--- spec/request/admin/pages/sections_spec.rb
+++ spec/request/admin/pages/sections_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe 'Admin: Page Sections', type: :request do
   describe 'GET /admin/pages/sections' do
     it 'redirects to the combined pages+sections list' do
       get admin_pages_sections_path
-      expect( response ).to have_http_status :found
+
+      expect( response      ).to have_http_status :found
       follow_redirect!
-      expect( response ).to have_http_status :ok
+      expect( response      ).to have_http_status :ok
       expect( response.body ).to include 'List pages'
     end
   end
@@ -14,7 +15,8 @@ RSpec.describe 'Admin: Page Sections', type: :request do
   describe 'GET /admin/pages/section/new' do
     it 'loads the form to add a new section' do
       get admin_pages_section_new_path
-      expect( response ).to have_http_status :ok
+
+      expect( response      ).to have_http_status :ok
       expect( response.body ).to include 'Add new section'
     end
   end
@@ -24,7 +26,8 @@ RSpec.describe 'Admin: Page Sections', type: :request do
       post admin_pages_section_new_path, params: {
         'page_section[title]': 'Test'
       }
-      expect( response ).to have_http_status :ok
+
+      expect( response      ).to have_http_status :ok
       expect( response.body ).to include I18n.t 'section_create_failed'
     end
     it 'adds a new section when the form is submitted' do
@@ -33,9 +36,10 @@ RSpec.describe 'Admin: Page Sections', type: :request do
         'page_section[title]': 'Test',
         'page_section[slug]': 'test'
       }
-      expect( response ).to have_http_status :found
+
+      expect( response      ).to have_http_status :found
       follow_redirect!
-      expect( response ).to have_http_status :ok
+      expect( response      ).to have_http_status :ok
       expect( response.body ).to include 'Edit section'
     end
   end
@@ -43,8 +47,10 @@ RSpec.describe 'Admin: Page Sections', type: :request do
   describe 'GET /admin/pages/section/:id' do
     it 'loads the form to edit an existing section' do
       section = create :page_section
+
       get admin_pages_section_path( section )
-      expect( response ).to have_http_status :ok
+
+      expect( response      ).to have_http_status :ok
       expect( response.body ).to include 'Edit section'
     end
   end
@@ -52,20 +58,24 @@ RSpec.describe 'Admin: Page Sections', type: :request do
   describe 'POST /admin/pages/section/:id' do
     it 'fails to update the section when submitted without all the details' do
       section = create :page_section
+
       post admin_pages_section_path( section ), params: {
         'page_section[name]': nil
       }
-      expect( response ).to have_http_status :ok
+
+      expect( response      ).to have_http_status :ok
       expect( response.body ).to include I18n.t 'section_update_failed'
     end
     it 'updates the section when the form is submitted' do
       section = create :page_section
+
       post admin_pages_section_path( section ), params: {
         'page_section[name]': 'Updated by test'
       }
-      expect( response ).to have_http_status :found
+
+      expect( response      ).to have_http_status :found
       follow_redirect!
-      expect( response ).to have_http_status :ok
+      expect( response      ).to have_http_status :ok
       expect( response.body ).to include 'Updated by test'
     end
   end
--- spec/request/admin/pages_spec.rb
+++ spec/request/admin/pages_spec.rb
@@ -11,8 +11,10 @@ RSpec.describe 'Admin: Pages', type: :request do
       hidden_subpage = create :page_in_subsection, :hidden
       hidden_section = create :page_section, :hidden
       create :page, section: hidden_section
+
       get admin_pages_path
-      expect( response ).to have_http_status :ok
+
+      expect( response      ).to have_http_status :ok
       expect( response.body ).to include 'List pages'
       expect( response.body ).to include 'Top-level pages'
       expect( response.body ).to include page.name
@@ -26,7 +28,8 @@ RSpec.describe 'Admin: Pages', type: :request do
   describe 'GET /admin/page/new' do
     it 'loads the form to add a new page' do
       get admin_page_new_path
-      expect( response ).to have_http_status :ok
+
+      expect( response      ).to have_http_status :ok
       expect( response.body ).to include 'Add new page'
     end
   end
@@ -36,20 +39,23 @@ RSpec.describe 'Admin: Pages', type: :request do
       post admin_page_new_path, params: {
         'page[title]': 'Test'
       }
-      expect( response ).to have_http_status :ok
+
+      expect( response      ).to have_http_status :ok
       expect( response.body ).to include I18n.t 'page_create_failed'
     end
     it 'adds a new page when the form is submitted' do
       template = create :page_template
+
       post admin_page_new_path, params: {
         'page[name]': 'Test',
         'page[title]': 'Test',
         'page[slug]': 'test',
         'page[template_id]': template.id
       }
-      expect( response ).to have_http_status :found
+
+      expect( response      ).to have_http_status :found
       follow_redirect!
-      expect( response ).to have_http_status :ok
+      expect( response      ).to have_http_status :ok
       expect( response.body ).to include 'Edit page'
     end
   end
@@ -57,8 +63,10 @@ RSpec.describe 'Admin: Pages', type: :request do
   describe 'GET /admin/page/:id' do
     it 'loads the form to edit an existing page' do
       page = create :page
+
       get admin_page_path( page )
-      expect( response ).to have_http_status :ok
+
+      expect( response      ).to have_http_status :ok
       expect( response.body ).to include 'Edit page'
     end
   end
@@ -66,20 +74,24 @@ RSpec.describe 'Admin: Pages', type: :request do
   describe 'POST /admin/page/:id' do
     it 'fails to update the page when submitted without all the details' do
       page = create :page
+
       post admin_page_path( page ), params: {
         'page[name]': nil
       }
-      expect( response ).to have_http_status :ok
+
+      expect( response      ).to have_http_status :ok
       expect( response.body ).to include I18n.t 'page_update_failed'
     end
     it 'updates the page when the form is submitted' do
       page = create :page
+
       post admin_page_path( page ), params: {
         'page[name]': 'Updated by test'
       }
-      expect( response ).to have_http_status :found
+
+      expect( response      ).to have_http_status :found
       follow_redirect!
-      expect( response ).to have_http_status :ok
+      expect( response      ).to have_http_status :ok
       expect( response.body ).to include 'Updated by test'
     end
   end
