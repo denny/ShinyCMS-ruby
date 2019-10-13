68c309ae96cd2e33f0f6406d93008bc17c05294e
Denny de la Haye <2018@denny.me>
2019-10-13 06:36:30 +0100

    Add tests (and rescues) for bad create/edit/delete attempts

--- spec/request/admin/settings_spec.rb
+++ spec/request/admin/settings_spec.rb
@@ -18,9 +18,11 @@ RSpec.describe 'Admin: Site Settings', type: :request do
       }
 
       expect( response      ).to have_http_status :found
+      expect( response      ).to redirect_to admin_settings_path
       follow_redirect!
       expect( response      ).to have_http_status :ok
       expect( response.body ).to include 'Site Settings'
+      expect( response.body ).to include I18n.t 'setting_created'
       expect( response.body ).to include 'New Setting Is New'
     end
     it 'adds a new setting, with an empty string value' do
@@ -30,9 +32,11 @@ RSpec.describe 'Admin: Site Settings', type: :request do
       }
 
       expect( response      ).to have_http_status :found
+      expect( response      ).to redirect_to admin_settings_path
       follow_redirect!
       expect( response      ).to have_http_status :ok
       expect( response.body ).to include 'Site Settings'
+      expect( response.body ).to include I18n.t 'setting_created'
       expect( response.body ).to include 'New Setting Is Empty'
     end
     it 'adds a new setting, with an explicitly null value' do
@@ -42,10 +46,24 @@ RSpec.describe 'Admin: Site Settings', type: :request do
       }
 
       expect( response      ).to have_http_status :found
+      expect( response      ).to redirect_to admin_settings_path
       follow_redirect!
       expect( response      ).to have_http_status :ok
       expect( response.body ).to include 'Site Settings'
+      expect( response.body ).to include I18n.t 'setting_created'
       expect( response.body ).to include 'New Setting Is Null'
+    end
+    it 'attempting to add a new setting with no name fails gracefully' do
+      post admin_setting_create_path, params: {
+        'setting[value]': 'MADE OF FAIL!'
+      }
+
+      expect( response      ).to have_http_status :found
+      expect( response      ).to redirect_to admin_settings_path
+      follow_redirect!
+      expect( response      ).to have_http_status :ok
+      expect( response.body ).to include 'Site Settings'
+      expect( response.body ).to include I18n.t 'setting_create_failed'
     end
   end
 
@@ -58,6 +76,7 @@ RSpec.describe 'Admin: Site Settings', type: :request do
       delete admin_setting_delete_path( s2.id )
 
       expect( response      ).to     have_http_status :found
+      expect( response      ).to     redirect_to admin_settings_path
       follow_redirect!
       expect( response      ).to     have_http_status :ok
       expect( response.body ).to     include 'Site Settings'
@@ -65,6 +84,16 @@ RSpec.describe 'Admin: Site Settings', type: :request do
       expect( response.body ).to     include s1.name
       expect( response.body ).to     include s3.name
       expect( response.body ).not_to include s2.name
+    end
+    it 'attempting to delete a non-existent setting fails gracefully' do
+      delete admin_setting_delete_path( 999 )
+
+      expect( response      ).to     have_http_status :found
+      expect( response      ).to     redirect_to admin_settings_path
+      follow_redirect!
+      expect( response      ).to     have_http_status :ok
+      expect( response.body ).to     include 'Site Settings'
+      expect( response.body ).to     include I18n.t 'setting_delete_failed'
     end
   end
 
@@ -79,6 +108,7 @@ RSpec.describe 'Admin: Site Settings', type: :request do
       }
 
       expect( response      ).to have_http_status :found
+      expect( response      ).to redirect_to admin_settings_path
       follow_redirect!
       expect( response      ).to have_http_status :ok
       expect( response.body ).to include 'Site Settings'
