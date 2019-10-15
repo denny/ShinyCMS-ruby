f4b0cad7df02605214d661cf550e845d8c55b4d0
Denny de la Haye <2018@denny.me>
2019-10-15 22:57:46 +0100

    Tweak routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,13 @@ Rails.application.routes.draw do
   get 'pages/*slugs', to: 'pages#show_in_section'
 
   # Users
-  devise_for :users
+  devise_for  :users,
+              singular: 'user',
+              path: 'user',
+              path_names: {
+                sign_in: 'login',
+                sign_out: 'logout'
+              }
 
   # ========== ( Admin area ) ==========
   get 'admin', to: 'admin#index'
