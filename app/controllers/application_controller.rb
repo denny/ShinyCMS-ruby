2b080c0caa95ae91ad08e290f1fce071426f4845
Denny de la Haye <2018@denny.me>
2019-10-15 21:15:57 +0100

    Check passwords against leaked password database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # Base class for main site controllers
 class ApplicationController < ActionController::Base
   layout 'main_site'
+
+  def after_sign_in_path_for(resource)
+    set_flash_message! :alert, :warn_pwned if resource.respond_to?( :pwned? ) &&
+                                              resource.pwned?
+    super
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
+  devise :pwned_password unless Rails.env.test?
 end
