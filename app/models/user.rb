096feb7d2c75e6ddf5acd892ab7961848e42024e
Denny de la Haye <2018@denny.me>
2019-10-15 21:25:17 +0100

    Use a queue for registration (etc) emails. I think.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
   devise :pwned_password unless Rails.env.test?
+
+  def send_devise_notification( notification, *args )
+    devise_mailer.public_send( notification, self, *args ).deliver_later
+  end
 end
