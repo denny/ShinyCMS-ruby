1234c3c60c78b6511368cb0096a443908d92f190
Denny de la Haye <2019@denny.me>
2019-10-17 17:00:29 +0100

    Make username constraint re-usable elsewhere (for routes)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 # User model - Devise powered
 class User < ApplicationRecord
+  # Allowed characters for usernames: a-z A-Z 0-9 . _ -
+  USERNAME_REGEX = %r{[-_\.a-zA-Z0-9]+}.freeze
+  public_constant :USERNAME_REGEX
+  ANCHORED_USERNAME_REGEX = %r{\A#{USERNAME_REGEX}\z}.freeze
+  private_constant :ANCHORED_USERNAME_REGEX
+
   # Include default and most extra devise modules. Only :omniauthable not used.
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
@@ -10,7 +16,7 @@ class User < ApplicationRecord
   validates :username, presence:   true
   validates :username, uniqueness: true
   # Restrict the character set for usernames to letters, numbers, and - . _
-  validates :username, format: /\A[-_\.a-zA-Z0-9]+\z/
+  validates :username, format: ANCHORED_USERNAME_REGEX
 
   # Virtual attribute to allow authenticating by either username or email
   attr_writer :login
