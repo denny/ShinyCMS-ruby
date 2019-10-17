0adf309161d38f7da26a10391e970124670b90ec
Denny de la Haye <denny.d@38degrees.org.uk>
2019-10-17 14:45:27 +0100

    Confirm the new user by default

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :user do
-    username { Faker::Internet.unique.username }
-    email    { Faker::Internet.unique.email    }
-    password { Faker::Internet.password        }
+    username     { Faker::Internet.unique.username }
+    email        { Faker::Internet.unique.email    }
+    password     { Faker::Internet.password        }
+    confirmed_at { Time.current                    }
   end
 end
