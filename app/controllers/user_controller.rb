da821c92c7e4b2c02af65da42adceeb06a0d7962
Denny de la Haye <2019@denny.me>
2019-10-22 16:29:49 +0100

    Simplify routes config for user/devise features

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,8 @@
 # Controller for user features not provided by Devise - profile pages, etc
-class UserController < ApplicationController
+class UserController < Devise::RegistrationsController
   def index
     if user_signed_in?
-      @user_profile = current_user
-      render :show
+      redirect_to user_profile_path( current_user )
     else
       redirect_to user_login_path
     end
@@ -11,5 +10,11 @@ class UserController < ApplicationController
 
   def show
     @user_profile = User.find_by( username: params[ :username ] )
+  end
+
+  protected
+
+  def after_update_path_for( resource )
+    user_path( resource )
   end
 end
