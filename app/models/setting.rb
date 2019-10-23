4d68c2b14424df7b2eb7d92a30cbc6a89ee25426
Denny de la Haye <2019@denny.me>
2019-10-23 22:36:45 +0100

    Add methods to check if a setting is set to yes, or something resembling yes

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,10 +2,28 @@
 class Setting < ApplicationRecord
   validates :name, presence: true
 
+  SOUNDS_LIKE_YES = %w[ yes y true on ].freeze
+  private_constant :SOUNDS_LIKE_YES
+
+  # Instance methods
+
+  # Return true if a setting has a yes-ish value
+  def yes?
+    return true if value.downcase == I18n.t( 'yes' ).downcase
+    return true if SOUNDS_LIKE_YES.include? value.downcase
+
+    false
+  end
+
   # Class methods
 
   # Return the value of the specified setting
   def self.get( name )
     where( name: name).pick( :value )
+  end
+
+  # Find a setting and then return true if it sounds like a 'yes'
+  def self.yes?( name )
+    find_by( name: name)&.yes?
   end
 end
