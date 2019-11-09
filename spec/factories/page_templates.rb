1bc38b1fde2b1ab35bdd2e5fccdf4733116fcf16
Denny de la Haye <2018@denny.me>
2019-11-09 09:02:00 +0000

    Update template tests to allow for extra (WIP) templates. Sort template filenames for predictable (and human-friendly) behaviour. Attempt to use fixtures/templates for tests but not quite working for some reason. More sleep required. :)

--- spec/factories/page_templates.rb
+++ spec/factories/page_templates.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :page_template do
     name     { Faker::Science.unique.scientist }
-    filename { 'example' }
+    filename { 'an_example' }
   end
 end
--- spec/models/page_template_spec.rb
+++ spec/models/page_template_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
+require 'fileutils'
 
 RSpec.describe PageTemplate, type: :model do
+  orig_dir = Rails.root.join( 'app', 'views', 'pages', 'templates' )
+  temp_dir = Rails.root.join( 'app', 'views', 'pages', 'temp' )
+  test_dir = Rails.root.join( 'spec', 'fixtures', 'templates' )
+
+  # before :all do
+    # FileUtils.mv orig_dir, temp_dir
+    # FileUtils.mv test_dir, orig_dir
+  # end
+
+  # after :all do
+    # FileUtils.mv orig_dir, test_dir
+    # FileUtils.mv temp_dir, orig_dir
+  # end
+
   context '.pages' do
     it 'returns the associated pages' do
       template = create :page_template
@@ -12,7 +27,7 @@ RSpec.describe PageTemplate, type: :model do
 
   context '.file_exists?' do
     it 'returns true if the template file exists on the disk' do
-      template = create :page_template, filename: 'example'
+      template = create :page_template, filename: 'an_example'
       expect( template.file_exists? ).to eq true
     end
 
@@ -24,8 +39,8 @@ RSpec.describe PageTemplate, type: :model do
 
   context 'PageTemplate.available_templates' do
     it 'returns the names of the template files in pages/templates' do
-      expect( PageTemplate.available_templates.size  ).to eq 1
-      expect( PageTemplate.available_templates.first ).to eq 'example'
+      expect( PageTemplate.available_templates.size  ).to eq 6
+      expect( PageTemplate.available_templates.first ).to eq 'an_example'
     end
   end
 
