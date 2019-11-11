61f7a9c137f564d8dce37a557a0b01d4b5ce7c09
Denny de la Haye <denny.d@38degrees.org.uk>
2019-11-11 22:11:38 +0000

    No need to special-case default theme here any more

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,21 +6,14 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Yarn node_modules folder
 Rails.application.config.assets.paths << Rails.root.join( 'node_modules' )
-# ShinyCMS default layout images (including admin toolbar)
-Rails.application.config.assets.paths << Rails.root.join( 'shinycms', 'images' )
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
+# Precompile stylesheets for ShinyCMS admin area, and the admin toolbar
 Rails.application.config.assets.precompile += %w[
-  shinycms/main_site.css
   shinycms/admin_toolbar.scss
   shinycms/admin_area.scss
 ]
 
-# Look for a non-default theme_name, if one is set then load its assets
+# Assets for the main site theme (default/unstyled theme is 'shinycms')
 theme = Rails.application.config.theme_name
-unless theme == 'default'
-  Rails.application.config.assets.paths << Rails.root.join( theme, 'images' )
-  Rails.application.config.assets.precompile += %W[ #{theme}/index.css ]
-end
+Rails.application.config.assets.paths << Rails.root.join( theme, 'images' )
+Rails.application.config.assets.precompile += %W[ #{theme}/index.css ]
