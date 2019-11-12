a0cf779e67f5b0eecd7a37dbc8eb3c98a544f48e
Denny de la Haye <denny.d@38degrees.org.uk>
2019-11-12 21:27:13 +0000

    Fix tests for shared behaviour across elements (just format_name currently)

--- spec/models/page_template_element_spec.rb
+++ spec/models/page_template_element_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe PageTemplateElement, type: :model do
   it_should_behave_like Element do
-    let( :element ) { described_class }
+    let( :template ) { create :page_template }
+    let( :element  ) { create :page_template_element, template: template }
   end
 end
