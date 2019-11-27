4edf6b126b656db8caa0b904678d5972f9236eb6
Denny de la Haye <2019@denny.me>
2019-11-27 22:30:11 +0000

    Update seeds to match code. Although I am about to rip all that code out, so *shrug*

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,21 +4,25 @@
 # It is used to populate the databse when you create it with `rake db:setup`
 # You can also load it (and reload it) at any time using `rake db:seed`
 
-# Settings
-seed Setting, { name: I18n.t( 'settings.features.users.register' ) }, {
-  value: 'No',
+## Settings
+
+# Feature flags
+seed Setting, { name: I18n.t( 'settings.features.users.can_register' ) }, {
+  value: 'Yes',
   description: "Set this to 'Yes' to allow users to create accounts"
 }
-seed Setting, { name: I18n.t( 'settings.features.users.login' ) }, {
-  value: 'No',
+seed Setting, { name: I18n.t( 'settings.features.users.can_login' ) }, {
+  value: 'Yes',
   description: "Set this to 'Yes' to allow users to log in"
 }
 
+# Restrict admin area access to specific IP addresses
 seed Setting, { name: I18n.t( 'settings.admin_ip_list' ) }, {
   value: '',
   description: 'Comma/space-separated list of IP addresses allowed to access admin area'
 }
 
+# Default page, default section. If page is set, section is ignored.
 seed Setting, { name: I18n.t( 'settings.default_page' ) }, {
   value: '',
   description: 'Default top-level page (either its name or its slug)'
