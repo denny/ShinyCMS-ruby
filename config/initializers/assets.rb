f3fcf651c5e003892ac98dbaa72809f26aff88ba
Denny de la Haye <2019@denny.me>
2019-12-02 15:21:39 +0000

    Move asset specification into manifest.js rather than assets initializer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,18 +6,3 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Yarn node_modules folder
 Rails.application.config.assets.paths << Rails.root.join( 'node_modules' )
-
-# Add images for admin toolbar to the pipeline
-base_image_path = Rails.root.join( 'app', 'assets', 'images' )
-Rails.application.config.assets.paths << base_image_path.join( 'shinycms' )
-
-# Precompile stylesheets for ShinyCMS admin area, and the admin toolbar
-Rails.application.config.assets.precompile += %w[
-  shinycms/admin_toolbar.scss
-  shinycms/admin_area.scss
-]
-
-# Assets for the main site theme (default/unstyled theme is 'shinycms')
-theme = Rails.application.config.theme_name
-Rails.application.config.assets.paths << base_image_path.join( theme )
-Rails.application.config.assets.precompile += %W[ #{theme}/index.css ]
