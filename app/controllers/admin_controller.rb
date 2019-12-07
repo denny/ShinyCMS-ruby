92ac6f844c76c580426b44011ae144c4da26e041
Denny de la Haye <2019@denny.me>
2019-12-07 22:17:06 +0000

    Shuffling stuff around for consistency across project

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,14 +1,17 @@
 # Base class for admin controllers
 class AdminController < ApplicationController
+  include Pundit
+
   before_action :check_admin_ip_list
   # before_action :authenticate_user!
 
-  include Pundit
-
   layout 'admin/layouts/admin_area'
 
-  # TODO: Redirect admins based on their ACL; 'Can edit news posts' to News, etc
+  # TODO: Add a user-setting so admins can set their preferred landing page
+  # TODO: If no preference is set, redirect based on their access:
+  #       can( 'List blog posts' ) to Blog admin section, etc
   def index
+    # redirect_to root_path unless current_user.can? 'View admin area'
     redirect_to admin_pages_path
   end
 
