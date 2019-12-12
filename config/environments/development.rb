443e6616db07ee74ba99cb8c6802d5a68a8bc6d3
Denny de la Haye <2019@denny.me>
2019-12-12 19:01:23 +0000

    Remove references to custom hostname code for S3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,7 @@ Rails.application.configure do
   # Store uploaded files on local file system (config/storage.yml for options)
   # config.active_storage.service = :local
   # Use AWS S3 to store files
-  # config.active_storage.service = :amazon
-  config.active_storage.service = :s3direct
+  config.active_storage.service = :amazon
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
