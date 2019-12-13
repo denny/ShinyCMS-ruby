547d4b178480588a135fe4c9e10d472449173e56
Denny de la Haye <2019@denny.me>
2019-12-13 21:42:50 +0000

    Massage data into expected shape, then use the default policy, for consistency with other auth

--- app/policies/admin/shared_content_policy.rb
+++ app/policies/admin/shared_content_policy.rb
@@ -1,6 +1,8 @@
 # Pundit policy for shared content admin area
 class Admin::SharedContentPolicy < Admin::DefaultPolicy
   def index?
-    @this_user.can? :list, :shared_content
+    @record = [ @record ]
+
+    super
   end
 end
