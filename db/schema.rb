58e95a4de2aefadcc2fdfbfe739d828c02308758
Denny de la Haye <2019@denny.me>
2019-12-17 19:01:40 +0000

    Add unique index to user capabilities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_17_005012) do
+ActiveRecord::Schema.define(version: 2019_12_17_175907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -151,6 +151,7 @@ ActiveRecord::Schema.define(version: 2019_12_17_005012) do
     t.integer "capability_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "capability_id"], name: "index_user_capabilities_on_user_id_and_capability_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
