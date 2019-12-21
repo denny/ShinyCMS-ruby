98e650ed3c2506397f4430d94b31c8dd865c096a
Denny de la Haye <2019@denny.me>
2019-12-21 23:17:00 +0000

    Put helper methods in alphabetical order

--- app/helpers/main_site_helper.rb
+++ app/helpers/main_site_helper.rb
@@ -4,11 +4,11 @@ module MainSiteHelper
     current_user&.can? capability, category
   end
 
-  def user_display_name( user = current_user )
-    user.display_name.presence || user.username
-  end
-
   def shared_content( name )
     SharedContentElement.where( name: name ).pick( :content )
+  end
+
+  def user_display_name( user = current_user )
+    user.display_name.presence || user.username
   end
 end
