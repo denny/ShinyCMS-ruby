d4947368685c889ca15e266a81a689347dc7832c
Denny de la Haye <2019@denny.me>
2019-12-22 00:01:55 +0000

    Use Faker to generate random fake data in factories

--- spec/factories/blog_posts.rb
+++ spec/factories/blog_posts.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :blog_post, class: 'BlogPost' do
-    title  { 'Post Title' }
-    slug   { 'post-slug'  }
-    body   { 'Post body goes here...' }
-    hidden { false        }
+    title  { Faker::Lorem.unique.sentence.titlecase }
+    slug   { Faker::Lorem.unique.word.downcase  }
+    body   { Faker::Lorem.paragraph }
+    hidden { false }
     posted_at { Time.zone.now }
 
     association :blog, factory: :blog
