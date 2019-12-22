c2f25486c4c741095fdfaa51cec7773df1c7f2f2
Denny de la Haye <2019@denny.me>
2019-12-22 22:57:17 +0000

    Missed a bit

--- spec/factories/settings.rb
+++ spec/factories/settings.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :setting do
     name  { "#{Faker::Science.unique.element}_#{Faker::Science.unique.element}".downcase }
-    value { Faker::Lorem.unique.scientist }
+    value { Faker::Science.unique.scientist }
   end
 end
