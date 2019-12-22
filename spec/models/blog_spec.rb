7d02f372620f8ae4baa61327969dbb4ad596fdd4
Denny de la Haye <2019@denny.me>
2019-12-22 01:03:53 +0000

    Poke some of the post-fetching methods on the Blog model

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -4,7 +4,35 @@ RSpec.describe Blog, type: :model do
   context 'factory' do
     it 'can create a blog' do
       blog = create :blog
+
       expect( Blog.first ).to eq blog
+    end
+
+    it 'can fetch the hidden posts' do
+      blog = create :blog
+      create :blog_post, blog: blog, hidden: true
+      create :blog_post, blog: blog
+      create :blog_post, blog: blog, hidden: true
+
+      expect( blog.hidden_posts.length ).to eq 2
+    end
+
+    it 'can fetch the non-hidden posts' do
+      blog = create :blog
+      create :blog_post, blog: blog, hidden: true
+      create :blog_post, blog: blog
+      create :blog_post, blog: blog, hidden: true
+
+      expect( blog.posts.length ).to eq 1
+    end
+
+    it 'can fetch all of the posts' do
+      blog = create :blog
+      create :blog_post, blog: blog, hidden: true
+      create :blog_post, blog: blog
+      create :blog_post, blog: blog, hidden: true
+
+      expect( blog.all_posts.length ).to eq 3
     end
   end
 end
