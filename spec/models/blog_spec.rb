86b022601c2d15d54204a92645e0bea91ff34301
Denny de la Haye <2019@denny.me>
2019-12-24 23:36:32 +0000

    Test slug/title generation methods

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -7,7 +7,9 @@ RSpec.describe Blog, type: :model do
 
       expect( Blog.first ).to eq blog
     end
+  end
 
+  context 'methods' do
     it 'can fetch the hidden posts' do
       blog = create :blog
       create :blog_post, blog: blog, hidden: true
@@ -33,6 +35,26 @@ RSpec.describe Blog, type: :model do
       create :blog_post, blog: blog, hidden: true
 
       expect( blog.all_posts.length ).to eq 3
+    end
+
+    it 'can create a title' do
+      blog = create :blog
+
+      blog.title = nil
+      expect( blog.title ).to be_blank
+
+      blog.generate_title
+      expect( blog.title ).to match %r{[-\w]+}
+    end
+
+    it 'can create a slug' do
+      blog = create :blog
+
+      blog.slug = nil
+      expect( blog.slug ).to be_blank
+
+      blog.generate_slug
+      expect( blog.slug ).to match %r{[-\w]+}
     end
   end
 end
