d166febcd9b61b510418a744b18f54852ba73981
Denny de la Haye <2019@denny.me>
2019-12-24 01:30:02 +0000

    Load WYSIWYG editor on blog new/edit pages

--- app/helpers/elements_helper.rb
+++ app/helpers/elements_helper.rb
@@ -16,6 +16,8 @@ module ElementsHelper
 
   # Return true if the page we're on might need a WYSIWYG HTML editor
   def html_editor_needed?
-    action_name == 'edit' || controller_name == 'shared_content'
+    controller_name == 'shared_content' ||
+      ( controller_name =~ /^blog/ && %w[ new edit ].include?( action_name )) ||
+      ( controller_name =~ /^page/ && %w[     edit ].include?( action_name ) )
   end
 end
