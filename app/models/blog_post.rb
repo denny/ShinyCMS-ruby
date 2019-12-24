b9478569d0343d9de28c547975188fb5adfe003f
Denny de la Haye <2019@denny.me>
2019-12-24 01:31:58 +0000

    Move view-blog-post-path method from model to helper; more conistent with other path helpers

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -5,16 +5,4 @@ class BlogPost < ApplicationRecord
 
   delegate :month, to: :posted_at
   delegate :year,  to: :posted_at
-
-  # Instance methods
-
-  def view_path
-    if Blog.multiple_blogs_mode
-      # :nocov:
-      "/blog/#{blog.slug}/#{year}/#{month}/#{slug}"
-      # :nocov:
-    else
-      "/blog/#{year}/#{month}/#{slug}"
-    end
-  end
 end
