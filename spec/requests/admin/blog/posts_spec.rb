44b1dc6858dc49d547defc6aa1299b730aa9b979
Denny de la Haye <2019@denny.me>
2019-12-25 00:18:32 +0000

    Expand blog admin tests slightly

--- spec/requests/admin/blog/posts_spec.rb
+++ spec/requests/admin/blog/posts_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Admin::Blog::Posts', type: :request do
   before :each do
-    create :blog
+    @blog = create :blog
 
     admin = create :blog_admin
     sign_in admin
@@ -11,6 +11,24 @@ RSpec.describe 'Admin::Blog::Posts', type: :request do
   describe 'GET /admin/blog/posts' do
     it 'fetches the list of blog posts' do
       get admin_blog_posts_path
+
+      expect( response ).to have_http_status :ok
+    end
+  end
+
+  describe 'GET /admin/blog/posts/new' do
+    it 'loads the form to create a new blog post' do
+      get new_admin_blog_post_path
+
+      expect( response ).to have_http_status :ok
+    end
+  end
+
+  describe 'GET /admin/blog/posts/:id/edit' do
+    it 'loads the form to edit an existing blog post' do
+      post = create :blog_post, blog: @blog
+
+      get edit_admin_blog_post_path( post )
 
       expect( response ).to have_http_status :ok
     end
