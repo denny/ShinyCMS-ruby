eb31506a2388557eba5f7efa866e479ee99f4293
Denny de la Haye <2019@denny.me>
2019-12-26 21:45:05 +0000

    WIP on changing routing to be more resourceful. Step 1, BREAK ALL THE THINGS!!!!

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,6 @@ Rails.application.routes.draw do
     # ========== ( Main site ) ==========
     root to: 'pages#index'
 
-    # Pages
-    get 'pages',       to: 'pages#index'
-    get 'pages/*path', to: 'pages#show'
-
     # Blogs
     if Rails.application.config.multiple_blogs_mode
       # :nocov:
@@ -50,10 +46,9 @@ Rails.application.routes.draw do
                   password: '/user/account/password',
                   unlock: '/user/account/unlock'
                 }
-    get 'users',           to: 'users#index'
-    get 'user',            to: 'users#index'
-    get 'user/:username',  to: 'users#show',
-                           as: :user_profile,
+    get 'users',           to: 'users#index', as: :main_site_users_redirect
+    get 'user',            to: 'users#index', as: :main_site_user_redirect
+    get 'user/:username',  to: 'users#show',  as: :user_profile,
                            constraints: { username: User::USERNAME_REGEX }
 
     # ========== ( Admin area ) ==========
@@ -62,47 +57,36 @@ Rails.application.routes.draw do
     # CKEditor (WYSIWYG editor used on various admin pages)
     mount Ckeditor::Engine => '/admin/ckeditor'
 
-    namespace :admin do
-      # Pages
-      get    'pages',          to: 'pages#index'
-      get    'page/new',       to: 'pages#new'
-      post   'page/new',       to: 'pages#create'
-      get    'page/:id',       to: 'pages#edit',       as: :page
-      post   'page/:id',       to: 'pages#update'
-      delete 'page/:id',       to: 'pages#delete',     as: :page_delete
-
-      namespace :pages do
-        # Page sections
-        get    'sections',     to: 'sections#index'
-        get    'section/new',  to: 'sections#new'
-        post   'section/new',  to: 'sections#create'
-        get    'section/:id',  to: 'sections#edit',    as: :section
-        post   'section/:id',  to: 'sections#update'
-        delete 'section/:id',  to: 'sections#delete',  as: :section_delete
-
-        # Page templates
-        get    'templates',    to: 'templates#index'
-        get    'template/new', to: 'templates#new'
-        post   'template/new', to: 'templates#create'
-        get    'template/:id', to: 'templates#edit',   as: :template
-        post   'template/:id', to: 'templates#update'
-        delete 'template/:id', to: 'templates#delete', as: :template_delete
-      end
-
+    scope path: 'admin', module: 'admin' do
       # Blogs
-      resources :blogs
-      namespace :blog do
+      resources :blog
+      resources :blog do
         resources :posts
       end
 
+      # Pages
+      get  :pages, to: 'pages#index'
+      post :page,  to: 'pages#create'
+      resources :page, controller: :pages, except: %w[ index show create ]
+
+      scope path: :pages, module: :pages, as: :page do
+        post :section,   to: 'sections#create'
+        resources :section,  controller: :sections,
+                             except: %w[ index show create ]
+        get  :templates, to: 'templates#index'
+        post :template,  to: 'templates#create'
+        resources :template, controller: :templates,
+                             except: %w[ index show create ]
+      end
+
       # Shared Content
-      get    'shared-content',            to: 'shared_content#index',
-                                          as: :shared_content
-      post   'shared-content',            to: 'shared_content#update'
-      post   'shared-content/create',     to: 'shared_content#create',
-                                          as: :shared_content_create
-      delete 'shared-content/delete/:id', to: 'shared_content#delete',
-                                          as: :shared_content_delete
+      get    'shared-content',        to: 'shared_content#index',
+                                      as: :shared_content
+      post   'shared-content',        to: 'shared_content#update'
+      post   'shared-content/create', to: 'shared_content#create',
+                                      as: :shared_content_create
+      delete 'shared-content/:id',    to: 'shared_content#delete',
+                                      as: :shared_content_delete
 
       # Site settings
       get    'settings',           to: 'settings#index'
@@ -111,15 +95,13 @@ Rails.application.routes.draw do
       delete 'setting/delete/:id', to: 'settings#delete', as: :setting_delete
 
       # Users
-      get    'users',    to: 'users#index'
-      get    'user/new', to: 'users#new'
-      post   'user/new', to: 'users#create'
-      get    'user/:id', to: 'users#edit',   as: :user
-      post   'user/:id', to: 'users#update'
-      delete 'user/:id', to: 'users#delete', as: :user_delete
+      get  :users, to: 'users#index'
+      post :user,  to: 'users#create'
+      resources :user, controller: :users, except: %w[ index show create ]
     end
 
-    # The Ultimate Catch-All Route - passes through to page handler
+    # The Ultimate Catch-All Route! Passes through to page handler,
+    # so that we can have top-level pages - /foo instead of /pages/foo
     get '*path', to: 'pages#show'
   end
 end
