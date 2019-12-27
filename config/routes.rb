cff3aeeeb97cb22ebec43b095448684bfc6803fd
Denny de la Haye <2019@denny.me>
2019-12-27 17:08:56 +0000

    Update shared content admin code (and tests) to match new routing

--- config/routes.rb
+++ config/routes.rb
@@ -86,13 +86,11 @@ Rails.application.routes.draw do
                               as: :create_page_template
 
       # Shared Content
-      get    'shared-content',        to: 'shared_content#index',
-                                      as: :shared_content
-      post   'shared-content',        to: 'shared_content#update'
-      post   'shared-content/create', to: 'shared_content#create',
-                                      as: :shared_content_create
-      delete 'shared-content/:id',    to: 'shared_content#delete',
-                                      as: :shared_content_delete
+      get    'shared-content',      to: 'shared_content#index'
+      put    'shared-content',      to: 'shared_content#update'
+      post   'shared-content',      to: 'shared_content#create'
+      delete 'shared-content/:id',  to: 'shared_content#destroy',
+                                    as: :delete_shared_content
 
       # Site settings
       get    'settings',           to: 'settings#index'
