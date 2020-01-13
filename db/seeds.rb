ff4edd2ad9cae0bb68aaa9e467437cb83f4d1590
Denny de la Haye <2020@denny.me>
2020-01-13 22:31:16 +0000

    Shuffle file into something resembling alphabetical order - no changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,47 +4,7 @@
 # It is used to populate the databse when you create it with `rails db:setup`
 # You can also load it (and reload it) at any time using `rails db:seed`
 
-# Settings (unset; just here to let people know that they're available)
-# TODO: replace this with the half-planned site/admin/user overrideable thing
-seed Setting, { name: I18n.t( 'admin.settings.admin_ip_list' ) }, {
-  value: '',
-  description: 'Comma/space-separated list of IP addresses allowed to access admin area'
-}
-seed Setting, { name: I18n.t( 'admin.settings.default_page' ) }, {
-  value: '',
-  description: 'Default top-level page (either its name or its slug)'
-}
-seed Setting, { name: I18n.t( 'admin.settings.default_section' ) }, {
-  value: '',
-  description: 'Default top-level section (either its name or its slug)'
-}
-
-# Inserts - these just need an empty InsertSet to group them
-seed InsertSet, { id: 1 }
-
-# Feature Flags (to turn on/off areas of site functionality)
-seed FeatureFlag, { name: 'blogs' }, {
-  description: '',
-  enabled: true,
-  enabled_for_admins: true
-}
-seed FeatureFlag, { name: 'user_login' }, {
-  description: 'Allow users to log in',
-  enabled: false,
-  enabled_for_admins: true
-}
-seed FeatureFlag, { name: 'user_profiles' }, {
-  description: 'Allow viewing of user profiles',
-  enabled: true,
-  enabled_for_admins: true
-}
-seed FeatureFlag, { name: 'user_registration' }, {
-  description: 'Allow users to create accounts',
-  enabled: true,
-  enabled_for_admins: true
-}
-
-# Capabilities (for user authorisation via Pundit)
+# Capabilities (for user authorisation, via Pundit)
 general_cc    = seed CapabilityCategory, { name: 'general'        }
 blogs_cc      = seed CapabilityCategory, { name: 'blogs'          }
 blog_posts_cc = seed CapabilityCategory, { name: 'blog_posts'     }
@@ -52,9 +12,9 @@ inserts_cc    = seed CapabilityCategory, { name: 'inserts'        }
 pages_cc      = seed CapabilityCategory, { name: 'pages'          }
 sections_cc   = seed CapabilityCategory, { name: 'page_sections'  }
 templates_cc  = seed CapabilityCategory, { name: 'page_templates' }
+settings_cc   = seed CapabilityCategory, { name: 'settings'       }
 users_cc      = seed CapabilityCategory, { name: 'users'          }
 admins_cc     = seed CapabilityCategory, { name: 'admin_users'    }
-settings_cc   = seed CapabilityCategory, { name: 'settings'       }
 # General
 seed Capability, { name: 'view_admin_area'      }, { category: general_cc }
 seed Capability, { name: 'view_admin_dashboard' }, { category: general_cc }
@@ -90,6 +50,11 @@ seed Capability, { name: 'list',    category: templates_cc }
 seed Capability, { name: 'add',     category: templates_cc }
 seed Capability, { name: 'edit',    category: templates_cc }
 seed Capability, { name: 'destroy', category: templates_cc }
+# Site Settings
+seed Capability, { name: 'list',    category: settings_cc }
+seed Capability, { name: 'add',     category: settings_cc }
+seed Capability, { name: 'edit',    category: settings_cc }
+seed Capability, { name: 'destroy', category: settings_cc }
 # Users
 seed Capability, { name: 'list',    category: users_cc }
 seed Capability, { name: 'add',     category: users_cc }
@@ -101,13 +66,9 @@ seed Capability, { name: 'list',    category: admins_cc }
 seed Capability, { name: 'add',     category: admins_cc }
 seed Capability, { name: 'edit',    category: admins_cc }
 seed Capability, { name: 'destroy', category: admins_cc }
-# Site Settings
-seed Capability, { name: 'list',    category: settings_cc }
-seed Capability, { name: 'add',     category: settings_cc }
-seed Capability, { name: 'edit',    category: settings_cc }
-seed Capability, { name: 'destroy', category: settings_cc }
 
 # One Admin To Rule Them All
+# TODO: replace this with a rake task and/or an install script
 admin = seed User, { username: 'admin' }, {
   email: 'admin@example.com',
   password: 'I should change this password before I do anything else!'
@@ -116,3 +77,43 @@ admin.confirm
 Capability.all.each do |c|
   admin.user_capabilities.find_or_create_by( capability_id: c.id )
 end
+
+# Feature Flags (to turn on/off areas of site functionality)
+seed FeatureFlag, { name: 'blogs' }, {
+  description: 'Enable blog (or blogs) feature',
+  enabled: true,
+  enabled_for_admins: true
+}
+seed FeatureFlag, { name: 'user_login' }, {
+  description: 'Allow users to log in',
+  enabled: false,
+  enabled_for_admins: true
+}
+seed FeatureFlag, { name: 'user_profiles' }, {
+  description: 'Allow viewing of user profiles',
+  enabled: true,
+  enabled_for_admins: true
+}
+seed FeatureFlag, { name: 'user_registration' }, {
+  description: 'Allow users to create accounts',
+  enabled: true,
+  enabled_for_admins: true
+}
+
+# Inserts (these just need an InsertSet to exist, to tie them together)
+InsertSet.create! if InsertSet.first.blank?
+
+# Settings (unset; just here to let people know that they're available)
+# TODO: replace this with the half-planned site/admin/user overrideable thing
+seed Setting, { name: I18n.t( 'admin.settings.admin_ip_list' ) }, {
+  value: '',
+  description: 'Comma/space-separated list of IP addresses allowed to access admin area'
+}
+seed Setting, { name: I18n.t( 'admin.settings.default_page' ) }, {
+  value: '',
+  description: 'Default top-level page (either its name or its slug)'
+}
+seed Setting, { name: I18n.t( 'admin.settings.default_section' ) }, {
+  value: '',
+  description: 'Default top-level section (either its name or its slug)'
+}
