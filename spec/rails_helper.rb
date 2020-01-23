b6d695b3c9b4cfc9e2e31e9d3dc04554a952409c
Denny de la Haye <2020@denny.me>
2020-01-23 19:29:33 +0000

    Explicitly load capybara here

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,8 +11,7 @@ require 'rspec/rails'
 
 # Add additional requires below this line; Rails is not loaded until this point
 
-# TODO? Capybara readme says to do this, but I don't seem to need it currently
-# require 'capybara/rails'
+require 'capybara/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
