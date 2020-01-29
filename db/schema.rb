761f43d22cb08c18001fe39188319f011364f488
Denny de la Haye <2020@denny.me>
2020-01-29 20:30:10 +0000

    Add acts-as-taggable-on gem and its db tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_11_221111) do
+ActiveRecord::Schema.define(version: 2020_01_29_202527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -178,6 +178,33 @@ ActiveRecord::Schema.define(version: 2020_01_11_221111) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "taggings", id: :serial, force: :cascade do |t|
+    t.integer "tag_id"
+    t.string "taggable_type"
+    t.integer "taggable_id"
+    t.string "tagger_type"
+    t.integer "tagger_id"
+    t.string "context", limit: 128
+    t.datetime "created_at"
+    t.index ["context"], name: "index_taggings_on_context"
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.index ["taggable_id", "taggable_type", "context"], name: "taggings_taggable_context_idx"
+    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
+    t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
+    t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
+    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
+    t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
+  end
+
+  create_table "tags", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true
+  end
+
   create_table "user_capabilities", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "capability_id", null: false
@@ -232,6 +259,7 @@ ActiveRecord::Schema.define(version: 2020_01_11_221111) do
   add_foreign_key "page_template_elements", "page_templates", column: "template_id"
   add_foreign_key "pages", "page_sections", column: "section_id"
   add_foreign_key "pages", "page_templates", column: "template_id"
+  add_foreign_key "taggings", "tags"
   add_foreign_key "user_capabilities", "capabilities"
   add_foreign_key "user_capabilities", "users"
 end
