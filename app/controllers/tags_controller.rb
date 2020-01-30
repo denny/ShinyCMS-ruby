67ccd1395884628cd3bb6929c79d7a551daadae9
Denny de la Haye <2020@denny.me>
2020-01-30 13:51:16 +0000

    Get tag index and show actions working with all tagged content (in future), add tags menu

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,15 +3,25 @@ class TagsController < ApplicationController
   before_action :check_feature_flags
 
   def index
-    @tags = BlogPost.tag_counts_on( :tags )
+    @tags = ActsAsTaggableOn::Tag.all
   end
 
   def show
-    @tag = params[ :tag ]
-    @posts = BlogPost.tagged_with( @tag )
+    @tag_name = params[ :tag ]
+    @tag = ActsAsTaggableOn::Tag.find_by( name: @tag_name )
+    @content_types = tagged_content_types
+    @tagged_items = {}
+    @content_types.each do |type|
+      @tagged_items[ type ] = type.constantize.tagged_with( @tag.name )
+      # @tagged_items[ 'BlogPost' ] = BlogPost.tagged_with( @tag.name )
+    end
   end
 
   private
+
+  def tagged_content_types
+    %w[ BlogPost ]
+  end
 
   def check_feature_flags
     enforce_feature_flags :tags
