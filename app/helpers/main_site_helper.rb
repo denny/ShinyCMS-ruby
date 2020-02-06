69adc08e2729ec4c26b392a632b137da55f91f98
Denny de la Haye <2020@denny.me>
2020-02-06 20:03:39 +0000

    Tests for new settings admin page

--- app/helpers/main_site_helper.rb
+++ app/helpers/main_site_helper.rb
@@ -15,14 +15,7 @@ module MainSiteHelper
   end
 
   def setting( name )
-    return unless ( setting = Setting.find_by( name: name.to_s ) )
-
-    return setting.site_value if setting.level == 'site'
-    return setting.site_value unless user_signed_in?
-
-    setting.user_value(    current_user ) ||
-      setting.admin_value( current_user ) ||
-      setting.site_value
+    Setting.get( name, current_user )
   end
 
   def user_display_name( user = current_user )
--- spec/factories/settings.rb
+++ spec/factories/settings.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :setting do
     name   { "#{Faker::Science.unique.element}_#{Faker::Science.unique.element}".downcase }
     level  { 'site' }
-    locked { true   }
+    locked { false  }
   end
 end
