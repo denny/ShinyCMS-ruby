837f13f0ba42194983f2889a50170dab86fbf212
Denny de la Haye <2020@denny.me>
2020-02-07 20:01:48 +0000

    Better flagging of partial failures updating settings

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -8,8 +8,8 @@ class Admin::SettingsController < AdminController
   def update
     skip_authorization # TODO: this is obviously bad; refactoring v. soon!
 
-    updated = update_settings
-    if updated
+    all_updated = update_settings( settings_params )
+    if all_updated
       flash[ :notice ] = t( '.success' )
     else
       flash[ :alert ] = t( '.failure' )
@@ -19,32 +19,34 @@ class Admin::SettingsController < AdminController
 
   private
 
-  def update_settings
-    params = settings_params[ :settings ]
+  def update_settings( params )
+    flag = true
     params&.each_key do |key|
       next unless /value_(?<id>\d+)/ =~ key
 
-      return false unless update_setting( id, params )
+      flag = false unless update_setting( id, params )
     end
-    true
+    flag
   end
 
   def update_setting( id, params )
     setting = Setting.find( id )
     level = params[ "level_#{id}" ]
 
-    unless setting.level == level
+    unless level.present? && setting.level == level
       return false unless setting.update( level: level )
     end
 
     val = setting.values.find_by( user_id: nil )
-    value = params[ "value_#{id}" ]
+    value = params[ "value_#{id}" ] || ''
     return true if val.value == value
 
     val.update!( value: value )
   end
 
   def settings_params
-    params.permit( :authenticity_token, :commit, settings: {} )
+    params.permit(
+      :authenticity_token, :commit, :_method, settings: {}
+    )[ :settings ]
   end
 end
