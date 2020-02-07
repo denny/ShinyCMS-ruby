b746d371db98892f55c8898850a70bbcc82ac35c
Denny de la Haye <2020@denny.me>
2020-02-07 20:02:17 +0000

    Separate tests for updating level of locked setting and updating value of locked setting

--- spec/requests/admin/settings_spec.rb
+++ spec/requests/admin/settings_spec.rb
@@ -79,22 +79,48 @@ RSpec.describe 'Admin: Site Settings', type: :request do
       expect( response.body ).to have_field "settings[value_#{s1.id}]", with: 'Unchanging'
     end
 
-    it "won't update a locked setting" do
+    it "won't update the level of a locked setting" do
+      s1 = create :setting, name: 'admin_ip_list', locked: true
+      v1 = create :setting_value, setting_id: s1.id, value: '127.0.0.1, 1.2.3.4'
+
+      put settings_path, params: {
+        "settings[level_#{s1.id}]": 'user',
+        "settings[value_#{s1.id}]": v1.value
+      }
+
+      expect( response      ).to     have_http_status :found
+      expect( response      ).to     redirect_to settings_path
+      follow_redirect!
+      expect( response      ).to     have_http_status :ok
+      expect( response.body ).to     have_title I18n.t( 'admin.settings.index.title' ).titlecase
+      expect( response.body ).to     have_css '.alert-danger', text: I18n.t( 'admin.settings.update.failure' )
+      expect( response.body ).to     have_field "settings[level_#{s1.id}]",
+                                                id: "settings_level_#{s1.id}_site",
+                                                disabled: true,
+                                                checked: true
+      expect( response.body ).not_to have_field "settings[level_#{s1.id}]",
+                                                id: "settings_level_#{s1.id}_user",
+                                                disabled: true,
+                                                checked: true
+    end
+
+    it 'will update the value of a locked setting' do
       s1 = create :setting, name: 'admin_ip_list', locked: true
       create :setting_value, setting_id: s1.id, value: '127.0.0.1, 1.2.3.4'
 
       put settings_path, params: {
-        "settings[level_#{s1.id}]": 'user',
+        "settings[level_#{s1.id}]": s1.level,
         "settings[value_#{s1.id}]": '127.0.0.1, 4.3.2.1'
       }
 
-      expect( response      ).to have_http_status :found
-      expect( response      ).to redirect_to settings_path
+      expect( response      ).to     have_http_status :found
+      expect( response      ).to     redirect_to settings_path
       follow_redirect!
-      expect( response      ).to have_http_status :ok
-      expect( response.body ).to have_title I18n.t( 'admin.settings.index.title' ).titlecase
-      expect( response.body ).to have_css '.alert-danger', text: I18n.t( 'admin.settings.update.failure' )
-      expect( response.body ).to have_field "settings[value_#{s1.id}]", with: '127.0.0.1, 1.2.3.4'
+      expect( response      ).to     have_http_status :ok
+      expect( response.body ).to     have_title I18n.t( 'admin.settings.index.title' ).titlecase
+      expect( response.body ).to     have_css '.alert-success', text: I18n.t( 'admin.settings.update.success' )
+      expect( response.body ).to     have_field "settings[value_#{s1.id}]", with: '127.0.0.1, 4.3.2.1'
+      expect( response.body ).not_to have_field "settings[value_#{s1.id}]", with: '127.0.0.1, 1.2.3.4'
     end
   end
 end
