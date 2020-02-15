48fc866bdd4c0f0fa9e132b8c7d3c29962ecd3ab
Denny de la Haye <2019@denny.me>
2020-02-15 05:26:02 +0000

    Refactor recaptcha code, some into helper, add invisible V2 check if no V3 key

--- app/helpers/recaptcha_helper.rb
+++ app/helpers/recaptcha_helper.rb
@@ -2,6 +2,24 @@
 module RecaptchaHelper
   include MainSiteHelper
 
+  def minimum_score( action )
+    name = "recaptcha_v3_#{action}_score"
+    setting( name.to_sym ) || ENV[ name.capitalize ] || 0.5
+  end
+
+  def verify_checkbox_recaptcha
+    verify_recaptcha_v2
+  end
+
+  def verify_invisible_recaptcha( action )
+    # Used in spec/requests/user_spec.rb, to test fallback to checkbox
+    return false if ENV[ 'FAIL_RECAPTCHA' ]
+
+    return verify_recaptcha_v3( action ) if ENV[ 'RECAPTCHA_V3_SITE_KEY' ]
+
+    verify_recaptcha_v2
+  end
+
   def verify_recaptcha_v2
     verify_recaptcha(
       secret_key: ENV[ 'RECAPTCHA_V2_SECRET_KEY' ],
@@ -9,16 +27,14 @@ module RecaptchaHelper
     )
   end
 
-  def verify_recaptcha_v3( action, score )
-    # The override is used in spec/requests/user_spec.rb
-    env = ENV[ 'OVERRIDE_RAILS_ENV' ] || ENV[ 'RAILS_ENV' ]
+  def verify_recaptcha_v3( action )
+    min_score = minimum_score( action ).to_f
 
     verify_recaptcha(
       action: action,
-      minimum_score: score.to_f,
+      minimum_score: min_score,
       secret_key: ENV[ 'RECAPTCHA_V3_SECRET_KEY' ],
-      site_key: ENV[ 'RECAPTCHA_V3_SITE_KEY' ],
-      env: env
+      site_key: ENV[ 'RECAPTCHA_V3_SITE_KEY' ]
     )
   end
 end
