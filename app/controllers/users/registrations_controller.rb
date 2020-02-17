0be80581d0577c9134acb9b02920a979fdb34937
Denny de la Haye <2020@denny.me>
2020-02-17 20:43:58 +0000

    Fallback flow didn't work with devise; switched from render to redirect

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,20 +5,22 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_feature_flags, only: %i[ new create ]
 
   def new
-    stash_recaptcha_key
+    @recaptcha_v3_key = ENV[ 'RECAPTCHA_V3_SITE_KEY' ]
+    @recaptcha_v2_key = ENV[ 'RECAPTCHA_V2_SITE_KEY' ]
     super
   end
 
   def create
-    stash_recaptcha_key
     invisible_success = verify_invisible_recaptcha( 'registration' )
     checkbox_success  = verify_checkbox_recaptcha unless invisible_success
 
     if invisible_success || checkbox_success
       super
     else
-      @show_checkbox_recaptcha = true
-      render :new
+      flash[ :show_checkbox_recaptcha ] = true
+      flash[ :username ] = params[ :username ]
+      flash[ :email    ] = params[ :email    ]
+      redirect_to action: :new
     end
   end
 
@@ -26,11 +28,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def after_update_path_for( _resource )
     edit_user_registration_path
-  end
-
-  def stash_recaptcha_key
-    @recaptcha_v3_key = ENV[ 'RECAPTCHA_V3_SITE_KEY' ]
-    @recaptcha_v2_key = ENV[ 'RECAPTCHA_V2_SITE_KEY' ] unless @recaptcha_v3_key
   end
 
   def check_feature_flags
