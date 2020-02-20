58bc25f67375bb9702054eab8ec12791e8a7da8a
Denny de la Haye <2019@denny.me>
2020-02-20 20:32:41 +0000

    Always stash both recaptcha keys (v2 and v3) if available

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,14 +3,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   include RecaptchaHelper
 
   before_action :check_feature_flags, only: %i[ new create ]
+  before_action :stash_recaptcha_keys
 
   def new
-    stash_recaptcha_key
     super
   end
 
   def create
-    stash_recaptcha_key
     invisible_success = verify_invisible_recaptcha( 'registration' )
     checkbox_success  = verify_checkbox_recaptcha unless invisible_success
 
@@ -28,9 +27,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
     edit_user_registration_path
   end
 
-  def stash_recaptcha_key
+  def stash_recaptcha_keys
     @recaptcha_v3_key = ENV[ 'RECAPTCHA_V3_SITE_KEY' ]
-    @recaptcha_v2_key = ENV[ 'RECAPTCHA_V2_SITE_KEY' ] unless @recaptcha_v3_key
+    @recaptcha_v2_key = ENV[ 'RECAPTCHA_V2_SITE_KEY' ]
   end
 
   def check_feature_flags
