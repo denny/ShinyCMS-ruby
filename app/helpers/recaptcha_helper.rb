ba66e586cb27ed00cbeea15c3489225796a4352a
Denny de la Haye <2020@denny.me>
2020-02-27 20:06:28 +0000

    Reorganise reCAPTCHA helper to put main methods above supporting methods

--- app/helpers/recaptcha_helper.rb
+++ app/helpers/recaptcha_helper.rb
@@ -1,37 +1,46 @@
-# Helper methods for turning CMS features on/off selectively
+# Helper methods for using Google reCAPTCHA
 module RecaptchaHelper
   include MainSiteHelper
-
-  def minimum_score( action )
-    name = "recaptcha_v3_#{action}_score"
-    setting( name.to_sym ) || ENV[ name.capitalize ] || 0.5
-  end
 
   def verify_checkbox_recaptcha
     verify_recaptcha_v2
   end
 
-  def verify_invisible_recaptcha( action )
-    return verify_recaptcha_v3( action ) if ENV[ 'RECAPTCHA_V3_SITE_KEY' ]
-
-    verify_recaptcha_v2
+  def verify_invisible_recaptcha( action = nil )
+    verify_recaptcha_v3( action ) || verify_recaptcha_v2
   end
 
   def verify_recaptcha_v2
+    return if ENV['RECAPTCHA_V2_SECRET_KEY'].blank?
+
     verify_recaptcha(
-      secret_key: ENV[ 'RECAPTCHA_V2_SECRET_KEY' ],
-      site_key: ENV[ 'RECAPTCHA_V2_SITE_KEY' ]
+      secret_key: ENV['RECAPTCHA_V2_SECRET_KEY'],
+      site_key: ENV['RECAPTCHA_V2_SITE_KEY']
     )
   end
 
   def verify_recaptcha_v3( action )
+    return if action.blank?
+    return if ENV['RECAPTCHA_V3_SECRET_KEY'].blank?
+
     min_score = minimum_score( action ).to_f
 
     verify_recaptcha(
       action: action,
       minimum_score: min_score,
-      secret_key: ENV[ 'RECAPTCHA_V3_SECRET_KEY' ],
-      site_key: ENV[ 'RECAPTCHA_V3_SITE_KEY' ]
+      secret_key: ENV['RECAPTCHA_V3_SECRET_KEY'],
+      site_key: ENV['RECAPTCHA_V3_SITE_KEY']
     )
+  end
+
+  def minimum_score( action )
+    score_for_action = "RECAPTCHA_V3_#{action.capitalize}_SCORE"
+    general_score    = 'RECAPTCHA_V3_SCORE'
+
+    setting( score_for_action.downcase.to_sym ) ||
+      ENV[ score_for_action ]                   ||
+      setting( general_score.downcase.to_sym )  ||
+      ENV[ general_score ]                      ||
+      0.5
   end
 end
