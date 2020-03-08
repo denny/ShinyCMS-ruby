8d58dda8039507f812ecde2a3690430cace89abf
Denny de la Haye <2019@denny.me>
2020-03-08 12:03:04 +0000

    Fix rubocop complaints

--- app/models/ahoy/event.rb
+++ app/models/ahoy/event.rb
@@ -1,7 +1,8 @@
+# Model for tracking website events using ahoy
 class Ahoy::Event < ApplicationRecord
   include Ahoy::QueryMethods
 
-  self.table_name = "ahoy_events"
+  self.table_name = 'ahoy_events'
 
   belongs_to :visit
   belongs_to :user, optional: true
--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -1,6 +1,7 @@
+# Model for tracking website visits using ahoy
 class Ahoy::Visit < ApplicationRecord
-  self.table_name = "ahoy_visits"
+  self.table_name = 'ahoy_visits'
 
-  has_many :events, class_name: "Ahoy::Event"
+  has_many :events, class_name: 'Ahoy::Event', dependent: :reject_with_error
   belongs_to :user, optional: true
 end
