ceedff7206035f53d54a5bf465c3ae78164c5261
Denny de la Haye <2020@denny.me>
2020-03-13 03:50:22 +0000

    Don't include spam comments in Discussion.comments

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -2,20 +2,12 @@
 class Discussion < ApplicationRecord
   belongs_to :resource, inverse_of: :discussion, polymorphic: true
 
-  has_many :comments, class_name: 'Comment',
-                      foreign_key: :discussion_id,
-                      inverse_of: :discussion,
-                      dependent: :destroy
+  has_many :comments, -> { where( spam: false ) },  class_name: 'Comment',
+                                                    foreign_key: :discussion_id,
+                                                    inverse_of: :discussion,
+                                                    dependent: :destroy
 
   # Instance methods
-
-  def discussion_hidden
-    hidden
-  end
-
-  def discussion_locked
-    locked
-  end
 
   def notification_email
     return if resource.blank?
