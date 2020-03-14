1e1db24a6ab982866fd7d213cacee5856d82994b
Denny de la Haye <2020@denny.me>
2020-03-14 02:39:55 +0000

    No need for realistic data in this simple helper test

--- spec/helpers/main_site_helper_spec.rb
+++ spec/helpers/main_site_helper_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe MainSiteHelper, type: :helper do
   describe 'setting' do
     it 'returns the setting value' do
-      s1 = create :setting, name: 'theme_name'
-      create :setting_value, setting_id: s1.id, value: 'TEST'
+      s1 = create :setting, name: 'testing_testing'
+      create :setting_value, setting_id: s1.id, value: '1 2 1 2'
 
-      expect( helper.setting( :theme_name ) ).to eq 'TEST'
+      expect( helper.setting( :testing_testing ) ).to eq '1 2 1 2'
     end
   end
 end
