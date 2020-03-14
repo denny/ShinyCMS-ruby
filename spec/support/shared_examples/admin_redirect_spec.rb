7f4faafe9a6f4414ea53cc3cee54b1293c71a237
Denny de la Haye <2020@denny.me>
2020-03-14 21:25:47 +0000

    Humanise output, improve variable name

--- spec/support/shared_examples/admin_redirect_spec.rb
+++ spec/support/shared_examples/admin_redirect_spec.rb
@@ -1,6 +1,6 @@
 # Reusable spec to check that each admin type gets redirected correctly on login
-RSpec.shared_examples '/admin redirect' do |type, path, str|
-  it "redirects a #{type} correctly" do
+RSpec.shared_examples '/admin redirect' do |type, path, section|
+  it "redirects a #{type.titlecase} correctly" do
     # rubocop:disable Rails/SaveBang
     admin = create type.to_sym
     sign_in admin
@@ -12,6 +12,6 @@ RSpec.shared_examples '/admin redirect' do |type, path, str|
     expect( response      ).to redirect_to url_for( path )
     follow_redirect!
     expect( response      ).to have_http_status :ok
-    expect( response.body ).to have_title I18n.t( "admin.#{str}.index.title" ).titlecase
+    expect( response.body ).to have_title I18n.t( "admin.#{section}.index.title" ).titlecase
   end
 end
