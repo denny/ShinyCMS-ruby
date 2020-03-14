57c91b488a28de156a67364e5c02c01be2a41a9d
Denny de la Haye <2020@denny.me>
2020-03-14 22:13:20 +0000

    Remove news/news_post translation here; consistency is king?

--- app/policies/admin/news_post_policy.rb
+++ app/policies/admin/news_post_policy.rb
@@ -1,31 +1,3 @@
 # Pundit policy for news post admin
-# Translates from :news_post' model to :news capability, for better UX (I hope)
 class Admin::NewsPostPolicy < Admin::DefaultPolicy
-  def index?
-    unless @record.class.method_defined?( :first ) && @record.first.present?
-      can_list_nil?
-    end
-
-    @this_user.can? :list, :news
-  end
-
-  def create?
-    @this_user.can? :add, :news
-  end
-
-  def new?
-    create?
-  end
-
-  def update?
-    @this_user.can? :edit, :news
-  end
-
-  def edit?
-    update?
-  end
-
-  def destroy?
-    @this_user.can? :destroy, :news
-  end
 end
