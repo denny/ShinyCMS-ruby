6ae0d113e4552614a3cb0c039b0fdade44e0bb1e
Denny de la Haye <2020@denny.me>
2020-03-15 23:20:35 +0000

    Remove comment moderation actions from discussion controller now that we have a separate comment admin controller

--- app/controllers/admin/discussions_controller.rb
+++ app/controllers/admin/discussions_controller.rb
@@ -10,7 +10,6 @@
 # ============================================================================
 class Admin::DiscussionsController < AdminController
   before_action :stash_discussion
-  before_action :stash_comment, except: %i[ hide unhide lock unlock ]
 
   def hide
     authorise @discussion
@@ -36,49 +35,9 @@ class Admin::DiscussionsController < AdminController
     redirect_to request.referer || discussion_path( @discussion )
   end
 
-  def hide_comment
-    authorise @comment
-    @comment.hide
-    redirect_to request.referer || discussion_path( @discussion )
-  end
-
-  def unhide_comment
-    authorise @comment
-    @comment.unhide
-    redirect_to request.referer || discussion_path( @discussion )
-  end
-
-  def lock_comment
-    authorise @comment
-    @comment.lock
-    redirect_to request.referer || discussion_path( @discussion )
-  end
-
-  def unlock_comment
-    authorise @comment
-    @comment.unlock
-    redirect_to request.referer || discussion_path( @discussion )
-  end
-
-  def delete_comment
-    authorise @comment
-    @comment.delete
-    redirect_to request.referer || discussion_path( @discussion )
-  end
-
-  def mark_comment_as_spam
-    authorise @comment
-    @comment.mark_as_spam
-    redirect_to request.referer || discussion_path( @discussion )
-  end
-
   private
 
   def stash_discussion
     @discussion = Discussion.find( params[ :id ] )
-  end
-
-  def stash_comment
-    @comment = @discussion.comments.find_by( number: params[ :number ] )
   end
 end
