b8a974785f29fcc6491b87c30347b4330fbff32c
Denny de la Haye <2020@denny.me>
2020-04-05 18:18:45 +0100

    Override Devise mailer, use MJML templates for password resets etc

--- app/mailer_previews/user_mailer_preview.rb
+++ app/mailer_previews/user_mailer_preview.rb
@@ -1,13 +1,33 @@
 # Rails Email Preview controller for previewing Devise-powered user emails
 class UserMailerPreview
-  def registration
-    UserMailer.registration fetch_user
+  def confirmation
+    UserMailer.registration fetch_user, fake_token
+  end
+
+  def reset_password
+    UserMailer.reset_password fetch_user, fake_token
+  end
+
+  def password_changed
+    UserMailer.password_changed fetch_user
+  end
+
+  def email_changed
+    UserMailer.email_changed fetch_user
+  end
+
+  def unlock_instructions
+    UserMailer.unlock_instructions fetch_user, fake_token
   end
 
   private
 
   def fetch_user
     @user_id ? User.find( @user_id ) : mock_user
+  end
+
+  def fake_token
+    'PREVIEW_TOKEN'
   end
 
   def mock_user
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,70 @@
 # Mailer for Devise-powered user account emails (password reset, etc)
 class UserMailer < Devise::Mailer
+  include Devise::Controllers::UrlHelpers
+
+  before_action :set_view_paths
   before_action :set_site_name
 
-  def registration( user )
+  default from: ->(*) { default_from_address }
+  default template_path: 'user_mailer'
+
+  def confirmation( user, token )
+    @resource = user
+    @token = token
+
+    mail(
+      to: user.email,
+      subject: t( '.subject' )
+    ) do |format|
+      format.mjml
+      format.text
+    end
+  end
+
+  def reset_password( user, token )
+    @resource = user
+    @token = token
+
+    mail(
+      to: user.email,
+      subject: t( '.subject' )
+    ) do |format|
+      format.mjml
+      format.text
+    end
+  end
+
+  def password_changed( user )
     @resource = user
 
     mail(
       to: user.email,
-      subject: t( '.subject' ),
-      template_path: 'shinycms/devise/mailer'
+      subject: t( '.subject' )
+    ) do |format|
+      format.mjml
+      format.text
+    end
+  end
+
+  def email_changed( user )
+    @resource = user
+
+    mail(
+      to: user.email,
+      subject: t( '.subject' )
+    ) do |format|
+      format.mjml
+      format.text
+    end
+  end
+
+  def unlock_instructions( user, token )
+    @resource = user
+    @token = token
+
+    mail(
+      to: user.email,
+      subject: t( '.subject' )
     ) do |format|
       format.mjml
       format.text
@@ -17,7 +73,18 @@ class UserMailer < Devise::Mailer
 
   private
 
+  def default_from_address
+    ENV[ 'MAILER_SENDER' ]
+  end
+
   def set_site_name
     @site_name = I18n.t( 'site_name' )
+  end
+
+  def set_view_paths
+    # Add the default templates directory to the top of view_paths
+    prepend_view_path 'app/views/shinycms'
+    # Apply the configured theme, if any, by adding it above the defaults
+    prepend_view_path Theme.current.view_path if Theme.current
   end
 end
