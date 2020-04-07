e77e7cff4c63b8bc97504eef8017a5f0fe89176c
Denny de la Haye <2020@denny.me>
2020-04-07 12:52:58 +0100

    Add test for REP index page

---
+++ spec/mailer_previews/rails_email_preview_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'RailsEmailPreview', type: :request do
+  before :each do
+    admin = create :email_admin
+    sign_in admin
+  end
+
+  describe 'when I load the index page' do
+    it 'it shows the list of emails' do
+      get rails_email_preview.rep_emails_path
+
+      expect( response.body )
+        .to have_content I18n.t( 'rails_email_preview.emails.index.list_title' )
+    end
+  end
+end
