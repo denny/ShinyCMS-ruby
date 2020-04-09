caa438a682c09e8da07067403ebcabea195c18e2
Denny de la Haye <2020@denny.me>
2020-04-09 15:26:11 +0100

    Fix main app / blazer routes to both work at same time

---
+++ app/lib/main_app_route_delegator.rb
@@ -0,0 +1,20 @@
+# Load ShinyCMS routes inside engines
+module MainAppRouteDelegator
+  def method_missing( method, *args, &block )
+    if main_app_route_method?( method )
+      main_app.__send__( method, *args )
+    else
+      super
+    end
+  end
+
+  def respond_to_missing?( method )
+    super || main_app_route_method?( method )
+  end
+
+  private
+
+  def main_app_route_method?( method )
+    method.to_s =~ /_(?:path|url)$/ && main_app.respond_to?( method )
+  end
+end
--- config/initializers/blazer.rb
+++ config/initializers/blazer.rb
@@ -1,7 +1,7 @@
 Rails.application.config.to_prepare do
   # Make Blazer appear inside our admin UI
-  # ::Blazer::BaseController.layout 'admin/layouts/admin_area'
+  ::Blazer::BaseController.layout 'admin/layouts/admin_area'
 
   # Make the main_app url helpers available to Blazer's views
-  # ::Blazer::BaseController.helper ::Rails.application.routes.url_helpers
+  ::Blazer::BaseController.helper MainAppRouteDelegator
 end
