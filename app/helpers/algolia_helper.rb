1ed1b1839c94f518d86337f6e0c590a549555fc8
Denny de la Haye <2020@denny.me>
2020-04-26 19:35:07 +0100

    Add some helper methods for Algolia

---
+++ app/helpers/algolia_helper.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Utility functions for dealing with Algolia (Search as a Service provider)
+module AlgoliaHelper
+  def algolia_is_enabled?
+    AlgoliaSearch&.configuration&.present?
+  end
+
+  # Algolia have a free plan for low-usage non-commercial sites. It requires you
+  # to display their logo on search result pages. If you are using a paid plan
+  # and you want to hide their logo, set ALGOLIASEARCH_USING_PAID_PLAN to 'Yes'.
+  def using_paid_algolia_plan?
+    ENV[ 'ALGOLIASEARCH_USING_PAID_PLAN' ].presence&.downcase == 'yes'
+  end
+
+  def using_free_algolia_plan?
+    !using_paid_algolia_plan
+  end
+end
