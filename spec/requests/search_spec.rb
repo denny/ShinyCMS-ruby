bea3f05aea0b54fb44698875973d68e18c735265
Denny de la Haye <2020@denny.me>
2020-04-27 21:55:18 +0100

    Start adding tests for search feature

---
+++ spec/requests/search_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Search:', type: :request do
+  describe 'GET /search' do
+    it 'displays the search form' do
+      get search_path
+
+      expect( response      ).to have_http_status :ok
+      expect( response.body ).to have_title I18n.t( 'search.form.title' )
+    end
+  end
+
+  describe 'GET /search?query=Success' do
+    it 'displays the search results' do
+      user = create :user, display_name: 'Success'
+
+      get "#{search_path}?query=Success"
+
+      expect( response      ).to     have_http_status :ok
+      expect( response.body ).to     have_title I18n.t( 'search.results.title', { query: 'Success' } )
+      expect( response.body ).to     include "/profile/#{user.username}"
+      expect( response.body ).not_to have_css 'p', text: I18n.t( 'search.results.no_results' )
+    end
+  end
+
+  describe 'GET /search?query=FAIL' do
+    it 'displays the lack of search results' do
+      user = create :user, display_name: 'Success'
+
+      get "#{search_path}?query=FAIL"
+
+      expect( response      ).to     have_http_status :ok
+      expect( response.body ).to     have_title I18n.t( 'search.results.title', { query: 'FAIL' } )
+      expect( response.body ).to     have_css 'p', text: I18n.t( 'search.results.no_results' )
+      expect( response.body ).not_to include "/profile/#{user.username}"
+    end
+  end
+end
