2ee861514d6bb5b87cd20f5145ae0df6f5716c45
Denny de la Haye <2020@denny.me>
2020-05-07 15:47:18 +0100

    Fix test for route delegator

--- spec/helpers/main_app_route_delegator_spec.rb
+++ spec/helpers/main_app_route_delegator_spec.rb
@@ -5,8 +5,8 @@ require 'rails_helper'
 RSpec.describe MainAppRouteDelegator, type: :helper do
   describe 'method_missing' do
     it 'fails correctly for a method which is missing from main app too' do
-      skip 'Bad test is bad and it should feel bad'
-      expect( ::Blazer::BaseController.respond_to_missing?( :b0rk ) ).to be false
+      expect { ::Blazer::BaseController.new.render( inline: '<%= w0rks? %>' ) }
+        .to raise_error ActionView::Template::Error
     end
   end
 end
