417656c9239583beb39201fcb5f48efc1c32361c
Denny de la Haye <2020@denny.me>
2020-07-30 21:02:36 +0100

    Add routing for Forms plugin

--- plugins/ShinyForms/config/routes.rb
+++ plugins/ShinyForms/config/routes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 ShinyForms::Engine.routes.draw do
-  resources :forms
+  post 'form/:slug', to: 'forms#process', as: :form
+
+  scope path: 'admin', module: 'admin' do
+    resources :forms, except: :show
+  end
 end
