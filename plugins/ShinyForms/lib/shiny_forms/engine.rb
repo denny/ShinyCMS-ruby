19451c045daace143828389c6f3406dd4a048c93
Denny de la Haye <2020@denny.me>
2020-07-30 18:00:20 +0100

    Hook FactoryBot up

--- plugins/ShinyForms/lib/shiny_forms/engine.rb
+++ plugins/ShinyForms/lib/shiny_forms/engine.rb
@@ -7,6 +7,8 @@ module ShinyForms
 
     config.generators do |g|
       g.test_framework :rspec
+      g.fixture_replacement :factory_bot
+      g.factory_bot dir: 'spec/factories'
     end
   end
 end
