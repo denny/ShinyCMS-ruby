54fd734854429f09c0fb8187e66f737d62209a1c
Denny de la Haye <2020@denny.me>
2020-07-30 21:02:18 +0100

    Strip out form handler templates for now, KISS

--- plugins/ShinyForms/app/models/shiny_forms/template.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module ShinyForms
-  # Model for ShinyCMS form templates
-  class Template < ApplicationRecord
-  end
-end
--- plugins/ShinyForms/spec/factories/template_elements.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :template_element, class: 'ShinyForms::TemplateElement' do
-    name { Faker::Books::CultureSeries.unique.civs.parameterize }
-    element_type { I18n.t( 'admin.elements.short_text' ) }
-  end
-end
