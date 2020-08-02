2013d1a0b9894d21ae94a71eed8db89d1ea4c714
Denny de la Haye <2020@denny.me>
2020-08-02 13:21:28 +0100

    Add tests for viewing form admin index page and loading form handler edit page

--- plugins/ShinyForms/spec/requests/admin/forms_spec.rb
+++ plugins/ShinyForms/spec/requests/admin/forms_spec.rb
@@ -8,12 +8,32 @@ RSpec.describe 'ShinyForms Admin', type: :request do
     sign_in admin
   end
 
+  describe 'GET /admin/forms' do
+    it 'loads the list of form handlers' do
+      get shiny_forms.forms_path
+
+      expect( response      ).to have_http_status :ok
+      expect( response.body ).to have_title I18n.t( 'shiny_forms.admin.forms.index.title' ).titlecase
+    end
+  end
+
   describe 'GET /admin/forms/new' do
     it 'loads the page to add a new form handler' do
       get shiny_forms.new_form_path
 
       expect( response      ).to have_http_status :ok
       expect( response.body ).to have_title I18n.t( 'shiny_forms.admin.forms.new.title' ).titlecase
+    end
+  end
+
+  describe 'GET /admin/forms/:id/edit' do
+    it 'loads the page to edit an existing form handler' do
+      form = create :form
+
+      get shiny_forms.edit_form_path( form )
+
+      expect( response      ).to have_http_status :ok
+      expect( response.body ).to have_title I18n.t( 'shiny_forms.admin.forms.edit.title' ).titlecase
     end
   end
 end
