a2442bb238c66d5cd6928cc7276be9a8bf485b69
Denny de la Haye <2020@denny.me>
2020-08-02 14:16:43 +0100

    Add method to extract submitted form params; remove unwanted index action

--- plugins/ShinyForms/app/controllers/shiny_forms/forms_controller.rb
+++ plugins/ShinyForms/app/controllers/shiny_forms/forms_controller.rb
@@ -7,15 +7,10 @@ module ShinyForms
   class FormsController < ApplicationController
     before_action :set_form, only: %i[ process ]
 
-    # GET /forms
-    def index
-      redirect_back fallback: root_path
-    end
-
-    # POST /forms/slug
+    # POST /forms/:slug
     def process
       if ShinyForms::FormHandler.respond_to? @form.handler
-        ShinyForms::FormHandler.public_send( @form.handler.symbolize, params )
+        ShinyForms::FormHandler.public_send( @form.handler.symbolize, shiny_form_params )
       else
         Rails.logger.warn "Unknown form handler (form ID: #{@form.id})"
         head :bad_request
@@ -24,9 +19,12 @@ module ShinyForms
 
     private
 
-    # Use callbacks to share common setup or constraints between actions.
     def set_form
-      @form = Form.find_by( params[:slug] )
+      @form = ShinyForms::Form.find_by( params[:slug] )
+    end
+
+    def shiny_form_params
+      params.require( :shiny_form ).permit( {} )
     end
   end
 end
