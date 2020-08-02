08c1351b25815fae15942148bccb4276581c728f
Denny de la Haye <2020@denny.me>
2020-08-02 01:52:26 +0100

    Add helper method to get capability category names for model instances

--- app/models/capability_category.rb
+++ app/models/capability_category.rb
@@ -4,4 +4,16 @@
 class CapabilityCategory < ApplicationRecord
   has_many :capabilities, inverse_of: :category, foreign_key: :category_id,
                           dependent: :restrict_with_error
+
+  # Class methods
+
+  # Returns the name of the capability category for a model instance, as a symbol
+  def self.name_for( record )
+    names = record.class.name.underscore.pluralize.split('/')
+    name = names.last
+
+    Rails.logger.debug "CapabilityCategory.name_for returned '#{name}' which doesn't exist" unless exists?( name: name )
+
+    name.to_sym
+  end
 end
