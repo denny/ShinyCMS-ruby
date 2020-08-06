bd6c082ae4d50c0d6f5d945fe1b0b54dd31c1dfb
Denny de la Haye <2020@denny.me>
2020-08-06 01:54:27 +0100

    Rewrite field_with_errors correctly in more (hopefully all) variations

--- config/initializers/field_with_errors.rb
+++ config/initializers/field_with_errors.rb
@@ -3,7 +3,13 @@
 # Set the 'field_with_errors' class on the input tag instead of wrapping it in a div
 ActionView::Base.field_error_proc =
   proc do |html_tag, _instance|
-    parts = html_tag.split('>', 2)
-    parts[0] += ' class="field_with_errors">'
-    (parts[0] + parts[1]).html_safe
+    if html_tag.match?( %r{class=['"][^'"]+['"]} )
+      html_tag.sub( %r{class=['"]([^'"]+)['"]}, 'class="\1 field_with_errors"' ).html_safe
+    elsif html_tag.include?( ' />' )
+      html_tag.sub( '/>', 'class="field_with_errors" />' ).html_safe
+    else
+      parts = html_tag.split( '>', 2 )
+      parts[0] += ' class="field_with_errors">'
+      (parts[0] + parts[1]).html_safe
+    end
   end
