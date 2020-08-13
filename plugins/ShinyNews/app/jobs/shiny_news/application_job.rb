fdd1e9ae816a586e0bf39429f9b271ed3d86f579
Denny de la Haye <2020@denny.me>
2020-08-13 02:41:33 +0100

    Remove unused job classes from news and profile plugins (keep in Forms for now, in case mailers need them)

--- plugins/ShinyNews/app/jobs/shiny_news/application_job.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module ShinyNews
-  class ApplicationJob < ActiveJob::Base
-  end
-end
--- plugins/ShinyProfiles/app/jobs/shiny_profiles/application_job.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module ShinyProfiles
-  class ApplicationJob < ActiveJob::Base
-  end
-end
