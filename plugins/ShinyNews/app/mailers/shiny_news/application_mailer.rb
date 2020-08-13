bbf8e14911443f76e610713a9b6cf641476178b1
Denny de la Haye <2020@denny.me>
2020-08-13 02:43:36 +0100

    Removing empty mailer classes from News and Profiles plugins

--- plugins/ShinyNews/app/mailers/shiny_news/application_mailer.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module ShinyNews
-  # Base mailer class for ShinyNews plugin for ShinyCMS
-  class ApplicationMailer < ActionMailer::Base
-  end
-end
--- plugins/ShinyProfiles/app/mailers/shiny_profiles/application_mailer.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module ShinyProfiles
-  # Base mailer class for ShinyProfiles plugin for ShinyCMS
-  class ApplicationMailer < ActionMailer::Base
-  end
-end
