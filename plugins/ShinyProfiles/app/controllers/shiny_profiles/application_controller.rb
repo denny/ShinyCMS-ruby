b019f9ac37c35f9bb8b1495f4d90e7eb6a354bbc
Denny de la Haye <2020@denny.me>
2020-08-23 17:27:42 +0100

    Update plugins to match main controller split in core

--- plugins/ShinyProfiles/app/controllers/shiny_profiles/application_controller.rb
+++ plugins/ShinyProfiles/app/controllers/shiny_profiles/application_controller.rb
@@ -1,20 +1,15 @@
 # frozen_string_literal: true
 
-# ============================================================================
-# Project:   ShinyProfiles plugin for ShinyCMS (Ruby version)
-# File:      plugins/ShinyProfiles/app/controllers/shiny_profiles/application_controller.rb
-# Purpose:   Base controller for main site
+# ShinyCMS ~ https://shinycms.org
 #
-# Copyright: (c) 2009-2020 Denny de la Haye https://denny.me
+# Copyright 2009-2020 Denny de la Haye ~ https://denny.me
 #
-# ShinyCMS is free software; you can redistribute it and/or
-# modify it under the terms of the GPL (version 2 or later).
-# ============================================================================
+# ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
 
 module ShinyProfiles
   # Main site base controller for ShinyProfiles plugin for ShinyCMS
-  # Inherits from ShinyCMS ApplicationController
-  class ApplicationController < ::MainController
+  # Inherits from ShinyCMS MainController
+  class MainController < ::MainController
     helper Rails.application.routes.url_helpers
   end
 end
--- plugins/ShinySearch/app/controllers/shiny_search/application_controller.rb
+++ plugins/ShinySearch/app/controllers/shiny_search/application_controller.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
+# ShinyCMS ~ https://shinycms.org
+#
+# Copyright 2009-2020 Denny de la Haye ~ https://denny.me
+#
+# ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
+
 module ShinySearch
-  # Inherits from ShinyCMS ApplicationController
-  class ApplicationController < ::MainController
+  # Main site base controller for ShinySearch plugin for ShinyCMS
+  # Inherits from ShinyCMS MainController
+  class MainController < ::MainController
     helper Rails.application.routes.url_helpers
   end
 end
