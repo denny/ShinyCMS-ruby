cee50018d081ebc90ccac8c50ba01397583a1b28
Denny de la Haye <2020@denny.me>
2020-08-29 04:50:48 +0100

    Fix factory namespace and names

--- plugins/ShinyLists/spec/factories/shiny_lists/subscriptions.rb
+++ plugins/ShinyLists/spec/factories/shiny_lists/subscriptions.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-# ShinyNewsletters plugin for ShinyCMS ~ https://shinycms.org
+# ShinyLists plugin for ShinyCMS ~ https://shinycms.org
 #
 # Copyright 2009-2020 Denny de la Haye ~ https://denny.me
 #
 # ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
 
-# Factory for list subscriptions - ShinyNewsletters plugin for ShinyCMS
+# Factory for list subscriptions - ShinyLists plugin for ShinyCMS
 FactoryBot.define do
-  factory :newsletter_subscription, class: 'ShinyNewsletters::Subscription' do
+  factory :mailing_list_subscription, class: 'ShinyLists::Subscription' do
   end
 end
