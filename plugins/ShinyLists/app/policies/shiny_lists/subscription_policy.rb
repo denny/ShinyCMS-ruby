cc5d259f0579555da81ea42ef2875c7f794d1025
Denny de la Haye <2020@denny.me>
2020-08-29 03:36:02 +0100

    Add pundit policies for lists and subscriptions

---
+++ plugins/ShinyLists/app/policies/shiny_lists/subscription_policy.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+# ShinyLists plugin for ShinyCMS ~ https://shinycms.org
+#
+# Copyright 2009-2020 Denny de la Haye ~ https://denny.me
+#
+# ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
+
+module ShinyLists
+  # Pundit policy for mailing lists - part of the ShinyLists plugin for ShinyCMS
+  class SubscriptionPolicy
+    attr_reader :this_user, :record
+
+    def initialize( this_user, record )
+      @this_user = this_user
+      @record = record
+    end
+
+    def index?
+      @this_user.can? :list, :mailing_list_subscriptions
+    end
+
+    def new?
+      @this_user.can? :add, :mailing_list_subscriptions
+    end
+
+    def create?
+      new?
+    end
+
+    def edit?
+      @this_user.can? :edit, :mailing_list_subscriptions
+    end
+
+    def update?
+      edit?
+    end
+
+    def destroy?
+      @this_user.can? :destroy, :mailing_list_subscriptions
+    end
+  end
+end
