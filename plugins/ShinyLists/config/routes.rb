a2752328b19378562edb26ab9c11be72f7259cbc
Denny de la Haye <2020@denny.me>
2020-08-29 14:58:19 +0100

    Add routes for lists and subscriptions

--- plugins/ShinyLists/config/routes.rb
+++ plugins/ShinyLists/config/routes.rb
@@ -10,6 +10,20 @@
 
 ShinyLists::Engine.routes.draw do
   scope format: false do
-    # ...
+    # Main site
+    get 'list/subscriptions/:token', to: 'subscriptions#index', as: :token_list_subscriptions
+    get 'list/subscriptions',        to: 'subscriptions#index', as: :user_list_subscriptions
+
+    post 'list/:slug/subscribe', to: 'subscriptions#create', as: :create_list_subscription
+
+    put 'list/:slug/unsubscribe/:token', to: 'subscriptions#unsubscribe', as: :token_list_unsubscribe
+    put 'list/:slug/unsubscribe',        to: 'subscriptions#unsubscribe', as: :user_list_unsubscribe
+
+    # Admin area
+    scope path: 'admin', module: 'admin' do
+      resources :lists, except: :show do
+        resources :subscriptions, except: :show
+      end
+    end
   end
 end
