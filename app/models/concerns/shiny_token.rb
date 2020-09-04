06bd3c08fba5171c4c49b721dc51e9eabc1fb72f
Denny de la Haye <2020@denny.me>
2020-09-04 18:35:07 +0100

    Add project comment banner to token concern

--- app/models/concerns/shiny_token.rb
+++ app/models/concerns/shiny_token.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+# ShinyCMS ~ https://shinycms.org
+#
+# Copyright 2009-2020 Denny de la Haye ~ https://denny.me
+#
+# ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
+
 # Common behaviour for models that have a uuid token attribute
 module ShinyToken
   extend ActiveSupport::Concern
