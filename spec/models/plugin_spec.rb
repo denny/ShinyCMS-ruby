bfc86cbfca1048fe06cef8efe5ce75c5ebc1ee0b
Denny de la Haye <2020@denny.me>
2020-09-11 00:45:11 +0100

    Add a couple more tests for Plugin model

--- spec/models/plugin_spec.rb
+++ spec/models/plugin_spec.rb
@@ -13,21 +13,27 @@ RSpec.describe Plugin, type: :model do
   context 'class methods' do
     describe '.loaded' do
       it 'returns an array of plugin instances' do
-        expect( Plugin.loaded ).to be_an Array
+        expect( Plugin.loaded       ).to be_an Array
         expect( Plugin.loaded.first ).to be_a Plugin
       end
     end
 
     describe '.loaded_names' do
       it 'returns an array of plugin names' do
-        expect( Plugin.loaded_names ).to be_an Array
+        expect( Plugin.loaded_names       ).to be_an Array
         expect( Plugin.loaded_names.first ).to be_a String
+      end
+    end
+
+    describe '.loaded?' do
+      it 'returns true if the named plugin is loaded' do
+        expect( Plugin.loaded?( 'ShinySearch' ) ).to be true
       end
     end
 
     describe '.models_with_demo_data' do
       it 'returns an array of model class names' do
-        expect( Plugin.models_with_demo_data ).to be_an Array
+        expect( Plugin.models_with_demo_data       ).to be_an Array
         expect( Plugin.models_with_demo_data.first ).to be_a String
       end
     end
@@ -35,13 +41,22 @@ RSpec.describe Plugin, type: :model do
 
   context 'instance methods' do
     describe '.models_with_demo_data' do
-      it 'returns an array of model class names' do
+      it 'returns an array of models' do
         plugin = Plugin.new( 'ShinyBlog' )
         models = plugin.models_with_demo_data
 
-        expect( models ).to be_an Array
-        expect( models.first ).to be_a String
-        expect( models.first ).to eq 'ShinyBlog::Post'
+        expect( models            ).to be_an Array
+        expect( models.first      ).to be_a Class
+        expect( models.first.name ).to eq 'ShinyBlog::Post'
+      end
+    end
+
+    describe '.admin_index_path' do
+      it 'returns the approriate path' do
+        plugin = Plugin.new( 'ShinyNewsletters' )
+        path = plugin.admin_index_path( 'editions' )
+
+        expect( path ).to be_a String
       end
     end
   end
