032834dcdfc670dbee19ba90b0230739308a0adc
Denny de la Haye <2020@denny.me>
2020-09-18 03:00:40 +0100

    Add jobs to send newletters, and tests for same

---
+++ plugins/ShinyNewsletters/app/jobs/shiny_newsletters/send_to_list_job.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# ShinyNewsletters plugin for ShinyCMS ~ https://shinycms.org
+#
+# Copyright 2009-2020 Denny de la Haye ~ https://denny.me
+#
+# ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
+
+module ShinyNewsletters
+  # Job to check for sends that are ready to go, and get them going :)
+  class SendToListJob < ApplicationJob
+    def perform( send )
+      return if send.sent?
+      return if send.sending?
+      return if send.future_dated?
+
+      send.update!( started_sending_at: Time.zone.now )
+
+      send.list.subscriptions.each do |subscription|
+        SendToSubscriberJob.perform_later( send, subscription.subscriber )
+      end
+
+      send.update!( finished_sending_at: Time.zone.now )
+    end
+  end
+end
---
+++ plugins/ShinyNewsletters/spec/jobs/shiny_newsletters/send_to_subscriber_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# ShinyNewsletters plugin for ShinyCMS ~ https://shinycms.org
+#
+# Copyright 2009-2020 Denny de la Haye ~ https://denny.me
+#
+# ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
+
+require 'rails_helper'
+
+# Tests for job that sends a single copy of a newsletter edition to a subscriber
+
+module ShinyNewsletters
+  RSpec.describe SendToSubscriberJob do
+    describe '.perform_later' do
+      it 'queues a send-to-subscriber job' do
+        ActiveJob::Base.queue_adapter = :test
+
+        subscriber1 = create :email_recipient
+        send1       = create :newsletter_send
+
+        expect { SendToSubscriberJob.perform_later( send1, subscriber1 ) }.to have_enqueued_job
+      end
+    end
+  end
+end
