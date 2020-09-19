9424c25448fd8c9f850b7d73a3c7ad64d321c308
Denny de la Haye <2020@denny.me>
2020-09-19 05:43:35 +0100

    Add paging helper and clean up controller code

---
+++ app/helpers/shiny_paging_helper.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# ShinyCMS ~ https://shinycms.org
+#
+# Copyright 2009-2020 Denny de la Haye ~ https://denny.me
+#
+# ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
+
+# Methods to help with paging
+module ShinyPagingHelper
+  def page_number
+    return 1 if params[:page].blank?
+    return 1 if params[:page].match?( /\D/ )
+
+    params[:page].to_i
+  end
+
+  # TODO: configurable per-site default, with per-user/per-action override
+  def items_per_page( default: 10 )
+    count = params[:count] || params[:size] || params[:per]
+
+    return default if count.blank?
+    return default if count.match?( /\D/ )
+
+    count.to_i
+  end
+end
