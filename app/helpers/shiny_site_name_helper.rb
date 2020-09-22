4056617da62397aa4836865657a63c6a55addf52
Denny de la Haye <2020@denny.me>
2020-09-22 22:46:48 +0100

    Move site_name helper into its own file and include it where needed

---
+++ app/helpers/shiny_site_name_helper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# ShinyCMS ~ https://shinycms.org
+#
+# Copyright 2009-2020 Denny de la Haye ~ https://denny.me
+#
+# ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
+
+# Helper to return your site name - included by ShinyMainSiteHelper and ShinyMailerHelper
+module ShinySiteNameHelper
+  def site_name
+    ::Setting.get( :site_name ) || I18n.t( 'site_name' )
+  end
+end
