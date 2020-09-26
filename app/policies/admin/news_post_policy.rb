f4ce635feb5edc837990124140bce8f77c947eb2
Denny de la Haye <2020@denny.me>
2020-09-26 01:01:54 +0100

    Remove policies for features that already moved to plugins

--- app/policies/admin/news_post_policy.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# Pundit policy for news post admin
-class Admin::NewsPostPolicy < Admin::DefaultPolicy
-end
--- app/policies/admin/page_section_policy.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# Pundit policy for page section admin
-class Admin::PageSectionPolicy < Admin::DefaultPolicy
-end
