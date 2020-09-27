8c53988b2eebb5e3947c1dedea9e664e5839758e
Denny de la Haye <2020@denny.me>
2020-09-27 22:01:47 +0100

    Add example form to fixtures & add test to exercise the new helper

---
+++ plugins/ShinyForms/app/helpers/shiny_forms/main_site_helper.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# ShinyForms plugin for ShinyCMS ~ https://shinycms.org
+#
+# Copyright 2009-2020 Denny de la Haye ~ https://denny.me
+#
+# ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
+
+module ShinyForms
+  # Helper method for finding form handlers - part of ShinyForms plugin for ShinyCMS
+  module MainSiteHelper
+    def find_form_by_slug( slug )
+      ShinyForms::Form.find_by( slug: slug )
+    end
+  end
+end
