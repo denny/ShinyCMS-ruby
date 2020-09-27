753068fee1a09416f45d5f797e3c8a04f009a3e7
Denny de la Haye <2020@denny.me>
2020-09-27 22:05:45 +0100

    Update template test to match new template count, and tidy up a bit

--- plugins/ShinyPages/spec/models/shiny_pages/template_spec.rb
+++ plugins/ShinyPages/spec/models/shiny_pages/template_spec.rb
@@ -4,39 +4,44 @@ require 'rails_helper'
 
 module ShinyPages
   RSpec.describe Template, type: :model do
-    context ' when I call .pages' do
-      it 'it returns the associated pages' do
-        template = create :page_template
-        create :page, template: template
-        create :page, template: template
-        expect( template.pages.size ).to eq 2
+    context 'instance methods' do
+      describe '.pages' do
+        it 'returns the associated pages' do
+          template = create :page_template
+          create :page, template: template
+          create :page, template: template
+          expect( template.pages.size ).to eq 2
+        end
+      end
+
+      describe '.file_exists?' do
+        it 'returns true if the template file exists on the disk' do
+          template = create :page_template, filename: 'an_example'
+          expect( template.file_exists? ).to eq true
+        end
+
+        it 'returns false if the template file does not exist' do
+          template = build :page_template, filename: 'NO-SUCH-FILE'
+          expect( template.file_exists? ).to eq false
+        end
+      end
+
+      describe '.elements' do
+        it 'returns the template elements' do
+          template = create :page_template
+
+          expect( template.elements.size ).to eq 4
+        end
       end
     end
 
-    context 'when I call .file_exists?' do
-      it 'it returns true if the template file exists on the disk' do
-        template = create :page_template, filename: 'an_example'
-        expect( template.file_exists? ).to eq true
-      end
-
-      it 'it returns false if the template file does not exist' do
-        template = build :page_template, filename: 'NO-SUCH-FILE'
-        expect( template.file_exists? ).to eq false
-      end
-    end
-
-    context 'when I call .elements' do
-      it 'it returns the template elements' do
-        template = create :page_template
-
-        expect( template.elements.size ).to eq 4
-      end
-    end
-
-    context 'when I call ShinyPages::Template.available_templates' do
-      it 'it returns the names of the template files in pages/templates' do
-        expect( ShinyPages::Template.available_templates.size  ).to eq 1
-        expect( ShinyPages::Template.available_templates.first ).to eq 'an_example'
+    context 'class methods' do
+      describe '.available_templates' do
+        it 'it returns the names of the template files in pages/templates' do
+          expect( ShinyPages::Template.available_templates.size  ).to eq 2
+          expect( ShinyPages::Template.available_templates.first ).to eq 'an_example'
+          expect( ShinyPages::Template.available_templates.last  ).to eq 'contact_form'
+        end
       end
     end
 
