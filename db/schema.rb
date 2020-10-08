fb7c38dafa46f519c060d453f801404c9fc34d2b
Denny de la Haye <2020@denny.me>
2020-10-09 00:55:29 +0100

    Add soft-delete columns to email_recipients table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_08_234213) do
+ActiveRecord::Schema.define(version: 2020_10_08_235031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -239,6 +239,8 @@ ActiveRecord::Schema.define(version: 2020_10_08_234213) do
     t.datetime "confirmed_at", precision: 6
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "deleted_at", precision: 6
+    t.index ["deleted_at"], name: "index_email_recipients_on_deleted_at"
     t.index ["email"], name: "index_email_recipients_on_email", unique: true
     t.index ["token"], name: "index_email_recipients_on_token", unique: true
   end
