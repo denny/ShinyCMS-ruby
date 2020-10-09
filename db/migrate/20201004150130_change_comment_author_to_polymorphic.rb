6846f949f5f10fe05e42a7770e7c50d6d6dde59b
Denny de la Haye <2020@denny.me>
2020-10-09 21:14:49 +0100

    Fix more migration errors

--- db/migrate/20201004150130_change_comment_author_to_polymorphic.rb
+++ db/migrate/20201004150130_change_comment_author_to_polymorphic.rb
@@ -1,15 +1,16 @@
 class ChangeCommentAuthorToPolymorphic < ActiveRecord::Migration[6.0]
   def change
-    Comment.where( author_type: 'anonymous'    ).update_all!( author_type: nil             )
-    Comment.where( author_type: 'pseudonymous' ).update_all!( author_type: 'CommentAuthor' )
-    Comment.where( author_type: 'verified'     ).update_all!( author_type: 'User'          )
+    Comment.where( author_type: 'anonymous'    ).update_all( author_type: nil             )
+    Comment.where( author_type: 'pseudonymous' ).update_all( author_type: 'CommentAuthor' )
+    Comment.where( author_type: 'verified'     ).update_all( author_type: 'User'          )
 
     Comment.where( author_type: 'CommentAuthor' ).each do |comment|
       author = CommentAuthor.create!(
         name: comment.author_name,
         website: comment.author_url,
+        ip_address: comment.ip_address
       )
-      comment.update!( author_id: author.id )
+      comment.update!( user_id: author.id )
 
       if comment.author_email.present?
         author.email_recipient.create!(
@@ -19,11 +20,11 @@ class ChangeCommentAuthorToPolymorphic < ActiveRecord::Migration[6.0]
       end
     end
 
-    drop_column :comments, :author_name
-    drop_column :comments, :author_email
-    drop_column :comments, :author_url
+    remove_column :comments, :author_name
+    remove_column :comments, :author_email
+    remove_column :comments, :author_url
 
-    rename column :comments, :user_id, :author_id
+    rename_column :comments, :user_id, :author_id
 
     add_index :comments, [ :author_id, :author_type ]
   end
