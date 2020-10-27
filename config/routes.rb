164ffeef640237b4cd10008ced62c5905ad275c8
Denny de la Haye <2020@denny.me>
2020-10-27 18:54:17 +0000

    WIP adding sidekiq-status extension for Sidekiq web dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -137,6 +137,8 @@ Rails.application.routes.draw do
     # Sidekiq Web provides a web dashboard for your sidekiq jobs and queues
     if sidekiq_web_enabled?
       require 'sidekiq/web'
+      require 'sidekiq/status'
+
       Sidekiq::Web.set :sessions, false
       authenticate :user, ->( user ) { user.can? :manage_sidekiq_jobs } do
         mount Sidekiq::Web, at: '/admin/sidekiq'
