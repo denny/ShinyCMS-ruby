d458db927fb000b97ac9230275d4d62875ead993
Denny de la Haye <2020@denny.me>
2020-11-02 13:43:51 +0000

    Remove unused policy checks for now

--- app/policies/email_recipient_policy.rb
+++ app/policies/email_recipient_policy.rb
@@ -24,14 +24,6 @@ class EmailRecipientPolicy
     index?
   end
 
-  def edit?
-    @this_user.can? :edit, :email_recipients
-  end
-
-  def update?
-    edit?
-  end
-
   def do_not_contact?
     destroy?
   end
