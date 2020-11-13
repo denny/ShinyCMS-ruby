c4c9e971a14c4892741ddd8a93f575526553177b
Denny de la Haye <2020@denny.me>
2020-11-13 11:02:03 +0000

    Plugin generator mostly passing rubocop now

--- lib/generators/shiny/plugin/plugin_generator.rb
+++ lib/generators/shiny/plugin/plugin_generator.rb
@@ -148,7 +148,7 @@ module Shiny
 
       def wrap_in_modules( unwrapped_code )
         unwrapped_code = unwrapped_code.to_s.strip.gsub( /\s$\n/, '' )
-        modules.reverse.inject( unwrapped_code ) do |content, mod|
+        modules.reverse.reduce( unwrapped_code ) do |content, mod|
           str = +"module #{mod}\n"
           str << content.lines.collect { |line| "  #{line}" }.join
           str << ( content.present? ? "\nend" : 'end' )
