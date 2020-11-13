b800db095a8c9a3209cb6744d2b19942cc56f232
Denny de la Haye <2020@denny.me>
2020-11-13 09:55:57 +0000

    Start cleaning up generator code; move utility methods into separate file

--- lib/generators/shiny/plugin/plugin_generator.rb
+++ lib/generators/shiny/plugin/plugin_generator.rb
@@ -6,80 +6,16 @@
 #
 # ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
 
+# Rails generator for ShinyCMS plugins (not quite finished, but it'll get you started)
+#
+# This is a hacked-about copy of the standard Rails Engine plugin generator
+# Some stuff that wasn't relevant has been removed
+# Some stuff to tie the resulting plugin into ShinyCMS has been added
+
 require 'rails/generators/rails/app/app_generator'
 require 'date'
 
-# Rails generator for ShinyCMS plugins (not quite finished, but it'll get you started)
-#
-# This is a hacked-about copy of the standard Rails Engine plugin generator, removing stuff
-# that wasn't relevant, and adding some helpful boilerplate to tie into ShinyCMS more easily
-module Rails
-  class PluginBuilder
-    def rakefile
-      template 'Rakefile'
-    end
-
-    def app
-      directory 'app'
-      empty_directory_with_keep_file "app/assets/images/#{namespaced_name}"
-    end
-
-    def readme
-      template 'README.md'
-    end
-
-    def gemfile
-      template 'Gemfile'
-    end
-
-    def license
-      template 'LICENSE'
-    end
-
-    def gemspec
-      template '%name%.gemspec'
-    end
-
-    def lib
-      template 'lib/%namespaced_name%.rb'
-      template 'lib/%namespaced_name%/engine.rb'
-      template 'lib/%namespaced_name%/version.rb'
-      template 'lib/tasks/%namespaced_name%_tasks.rake'
-    end
-
-    def config
-      template 'config/routes.rb'
-      template 'config/locales/en.yml'
-    end
-
-    def assets_manifest
-      template 'rails/engine_manifest.js', "app/assets/config/#{underscored_name}_manifest.js"
-    end
-
-    def stylesheets
-      copy_file 'rails/stylesheets.css',
-                "app/assets/stylesheets/#{namespaced_name}/application.css"
-    end
-
-    def bin(force = false)
-      bin_file = 'bin/rails.tt'
-      template bin_file, force: force do |content|
-        "#{shebang}\n" + content
-      end
-      chmod 'bin', 0755, verbose: false
-    end
-
-    def gemfile_entry
-      return unless inside_application?
-
-      gemfile_in_app_path = File.join(rails_app_path, 'Gemfile')
-      if File.exist? gemfile_in_app_path
-        entry = "\ngem '#{name}', path: '#{relative_path}'"
-        append_file gemfile_in_app_path, entry
-      end
-    end
-  end
-end
+require_relative './plugin_builder'
 
 module Shiny
   module Generators
@@ -167,7 +103,7 @@ module Shiny
           build(:test_dummy_assets)
           build(:test_dummy_clean)
           # ensure that bin/rails has proper dummy_path
-          build(:bin, true)
+          build(:bin, force: true)
         end
       end
 
