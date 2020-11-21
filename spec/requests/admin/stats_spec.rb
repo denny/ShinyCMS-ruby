ba82c87a62b9f8456f489182233a7483edaa95fa
Denny de la Haye <2020@denny.me>
2020-11-21 02:36:55 +0000

    Add a test for failing blazer auth check

--- spec/requests/admin/stats_spec.rb
+++ spec/requests/admin/stats_spec.rb
@@ -10,28 +10,48 @@ require 'rails_helper'
 
 # Tests for the Blazer integration
 RSpec.describe 'Blazer (charts and dashboards)', type: :request do
-  before :each do
-    admin = create :stats_admin
-    sign_in admin
-  end
+  context 'as a stats admin user' do
+    before :each do
+      admin = create :stats_admin
+      sign_in admin
+    end
 
-  describe 'GET /stats' do
-    it 'succeeds' do
-      get blazer_path
+    describe 'GET /stats' do
+      it 'succeeds' do
+        get blazer_path
 
-      expect( response      ).to have_http_status :ok
-      # FIXME: setting @page_title in _breadcrumbs.html.erb isn't being picked up by _head.html.erb
-      # expect( response.body ).to have_title I18n.t( 'admin.blazer.queries' )
-      expect( response.body ).to have_css 'a', text: I18n.t( 'admin.stats.breadcrumb' )
+        expect( response      ).to have_http_status :ok
+        # FIXME: setting @page_title in _breadcrumbs.html.erb isn't being picked up by _head.html.erb
+        # expect( response.body ).to have_title I18n.t( 'admin.blazer.queries' )
+        expect( response.body ).to have_css 'a', text: I18n.t( 'admin.stats.breadcrumb' )
+      end
+    end
+
+    describe 'GET /stats' do
+      it 'generates the correct button link' do
+        get blazer_path
+
+        expect( response      ).to have_http_status :ok
+        expect( response.body ).to have_link 'New Query', href: '/admin/stats/queries/new'
+      end
     end
   end
 
-  describe 'GET /stats' do
-    it 'generates the correct button link' do
+  context 'as an admin user without access to the stats feature' do
+    before :each do
+      admin = create :admin_user
+      sign_in admin
+    end
+
+    it 'does not allow access' do
       get blazer_path
 
-      expect( response      ).to have_http_status :ok
-      expect( response.body ).to have_link 'New Query', href: '/admin/stats/queries/new'
+      expect( response      ).to have_http_status :found
+      expect( response      ).to redirect_to '/admin'
+      # TODO: FIXME: Something about this redirect is weird; rspec won't follow it, claims it's nil
+      # follow_redirect!
+      # expect( response      ).to have_http_status :ok
+      # expect( response.body ).to have_css '.alerts', text: I18n.t( 'admin.blazer.auth_fail' )
     end
   end
 end
