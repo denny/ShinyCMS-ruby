d35192b864ebf7c52f3073f7a1cd483fbda382f6
Denny de la Haye <2020@denny.me>
2020-12-05 00:09:49 +0000

    Move admin toolbar assets, remove some unused admin assets/config

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -4,20 +4,10 @@
 //
 // ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
 
-// ========== Admin Area ==========
-
 //= link shinycms/admin_area.css
-//= link_tree ../images/shinycms/admin
-
-// ========== Admin Toolbar ==========
-
 //= link shinycms/admin_toolbar.css
 
-// ========== Main Site (default, un-themed templates) ==========
-
 //= link shinycms/main_site.css
-
-// ========== All of the above ==========
 
 //= link_tree ../images/shinycms
 
