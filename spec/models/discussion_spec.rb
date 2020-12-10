68368ca6ce7644eb126c8ce6763dfce20652d395
Denny de la Haye <2020@denny.me>
2020-12-10 01:16:14 +0000

    Simplify discussion model spec a bit

--- spec/models/discussion_spec.rb
+++ spec/models/discussion_spec.rb
@@ -10,56 +10,45 @@ require 'rails_helper'
 
 # Tests for discussion model
 RSpec.describe Discussion, type: :model do
-  describe 'factory' do
-    it 'can create a discussion' do
-      news_post  = create :news_post
-      discussion = create :discussion, resource: news_post
+  describe '.recently_active' do
+    before :each do
+      @active_last_week = create :news_post
+      @active_this_week = create :news_post
+      @less_active_post = create :news_post
+      @an_inactive_post = create :news_post
 
-      expect( Discussion.last ).to eq discussion
+      create :discussion, resource: @active_last_week, comment_count: 5, comments_posted_at: 8.days.ago
+      create :discussion, resource: @active_this_week, comment_count: 4
+      create :discussion, resource: @less_active_post, comment_count: rand( 1..3 )
+      create :discussion, resource: @an_inactive_post, comment_count: 0
     end
-  end
 
-  context 'methods' do
-    describe '.recently_active' do
-      before :each do
-        @active_last_week = create :news_post
-        @active_this_week = create :news_post
-        @less_active_post = create :news_post
-        @an_inactive_post = create :news_post
+    context 'without params' do
+      it 'fetches the most active discussions from the last week, most active first' do
+        active, counts = Discussion.recently_active
 
-        create :discussion, resource: @active_last_week, comment_count: 5, comments_posted_at: 8.days.ago
-        create :discussion, resource: @active_this_week, comment_count: 4
-        create :discussion, resource: @less_active_post, comment_count: rand( 1..3 )
-        create :discussion, resource: @an_inactive_post, comment_count: 0
+        expect( active.length ).to eq 2
+        expect( active.find( counts.to_h.keys.first ).resource ).to eq @active_this_week
       end
+    end
 
-      context 'without params' do
-        it 'fetches the most active discussions from the last week, most active first' do
-          active, counts = Discussion.recently_active
+    context 'with params' do
+      it 'fetches the most active discussions from the specified timespan' do
+        active, counts = Discussion.recently_active( days: 14 )
 
-          expect( active.length ).to eq 2
-          expect( active.find( counts.to_h.keys.first ).resource ).to eq @active_this_week
-        end
+        expect( active.length ).to eq 3
+        expect( active.find( counts.to_h.keys.first ).resource ).to eq @active_last_week
       end
+    end
 
-      context 'with params' do
-        it 'fetches the most active discussions from the specified timespan' do
-          active, counts = Discussion.recently_active( days: 14 )
+    context 'when discussions have no comments' do
+      it "doesn't include them" do
+        active, _counts = Discussion.recently_active
 
-          expect( active.length ).to eq 3
-          expect( active.find( counts.to_h.keys.first ).resource ).to eq @active_last_week
-        end
-      end
-
-      context 'when discussions have no comments' do
-        it "doesn't include them" do
-          active, _counts = Discussion.recently_active
-
-          expect( active.length ).to eq 2
-          # TODO: expect( active.resources ).not_to include @an_inactive_post
-          expect( active.first.resource  ).not_to eq @an_inactive_post
-          expect( active.second.resource ).not_to eq @an_inactive_post
-        end
+        expect( active.length ).to eq 2
+        # TODO: expect( active.resources ).not_to include @an_inactive_post
+        expect( active.first.resource  ).not_to eq @an_inactive_post
+        expect( active.second.resource ).not_to eq @an_inactive_post
       end
     end
   end
