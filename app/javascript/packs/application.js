f7d3eca91226bd017525192b4666489632b08b9d
Denny de la Haye <2020@denny.me>
2020-12-15 00:56:41 +0000

    Move commented-out smartmenus config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,8 +13,6 @@ require("@rails/activestorage").start()
 // require("channels") // Only needed if actioncable enabled
 require("jquery")
 
-// require("smartmenus")
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
