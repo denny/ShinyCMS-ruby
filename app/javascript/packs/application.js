f768f1dbf003e7381dd17383bea089f951a95b54
Denny de la Haye <2020@denny.me>
2020-12-16 19:59:06 +0000

    Add TODO/FIXME notes to JS config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-//require("@rails/ujs").start()
 import Rails from '@rails/ujs'
 global.Rails = Rails;
 Rails.start();
 
-// require("turbolinks").start()
-require("@rails/activestorage").start()
-// require("channels") // Only needed if actioncable enabled
-require("jquery")
+require( '@rails/activestorage' ).start()
 
+// Currently not needed (if enabled in future, may need @rails prefix)
+// require( 'turbolinks' ).start()
+// require( 'channels' )  // ActionCable
+
+require( 'jquery' )
+
+// TODO: Try serving CSS and images via webpacker at some point?
+//
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
