489a24f09959cc54bf999a821909541784c4d731
Denny de la Haye <2020@denny.me>
2021-01-17 20:22:06 +0000

    Never return anon comments

--- app/helpers/shiny_discussion_helper.rb
+++ app/helpers/shiny_discussion_helper.rb
@@ -17,6 +17,8 @@ module ShinyDiscussionHelper
   end
 
   def recent_comments_by_user( user, count = 10 )
+    return if user.nil?
+
     Comment.readonly.where( author: user ).recent.limit( count )
   end
 end
