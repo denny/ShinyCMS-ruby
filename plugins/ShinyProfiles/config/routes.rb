6fa745191d67ad6fffc391552ef6496d2e603975
Denny de la Haye <2020@denny.me>
2021-01-19 19:36:24 +0000

    Add update route, use constraint constant from User model

--- plugins/ShinyProfiles/config/routes.rb
+++ plugins/ShinyProfiles/config/routes.rb
@@ -11,11 +11,9 @@ ShinyProfiles::Engine.routes.draw do
     # Main site
     get 'profiles',               to: 'profiles#index'
     get 'profile',                to: 'profiles#profile_redirect', as: :profile_redirect
-    get 'profile/:username',      to: 'profiles#show', as: :profile,
-                                  constraints: { username: User::USERNAME_REGEX }
-    get 'profile/:username/edit', to: 'profiles#edit', as: :edit_profile,
-                                  constraints: { username: User::USERNAME_REGEX }
-    put 'profile/:username',      to: 'profiles#update'
+    get 'profile/:username',      to: 'profiles#show', as: :profile, constraints: User::USERNAME_ROUTE_CONSTRAINT
+    get 'profile/:username/edit', to: 'profiles#edit', as: :edit_profile, constraints: User::USERNAME_ROUTE_CONSTRAINT
+    put 'profile/:username',      to: 'profiles#update', constraints: User::USERNAME_ROUTE_CONSTRAINT
 
     # Admin area
     scope path: 'admin', module: 'admin' do
