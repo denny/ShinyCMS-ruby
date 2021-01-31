710e01ff5cb2ad72c5aafb495d4c0637503fa660
Denny de la Haye <2021@denny.me>
2021-01-31 22:23:48 +0000

    Instantiate an instance of the Sitemap class here

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
-# ShinySEO plugin for ShinyCMS ~ https://shinycms.org
+# ShinyCMS ~ https://shinycms.org
 #
 # Copyright 2009-2021 Denny de la Haye ~ https://denny.me
 #
 # ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
 
-# If a plugin wants to add items to the sitemap, the relevant model(s) should have
-# a `sitemap_items` class method which returns an array of `ShinySEO::SitemapItem`s,
-# and a `.path` instance method (which SitemapItem uses to build a link to the item)
-
-ShinySEO::Sitemap.generate
+ShinySEO::Sitemap.new.generate
