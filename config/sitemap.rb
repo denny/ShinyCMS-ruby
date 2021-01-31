9fe39600e47d18c0031c50acb03d780ed950cd2f
Denny de la Haye <2021@denny.me>
2021-01-31 14:54:48 +0000

    Add model to wrap sitemap_generator gem, for easier testing and integration

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -10,38 +10,4 @@
 # a `sitemap_items` class method which returns an array of `ShinySEO::SitemapItem`s,
 # and a `.path` instance method (which SitemapItem uses to build a link to the item)
 
-s3_access_key_id = ENV[ 'AWS_S3_FEEDS_ACCESS_KEY_ID' ]
-
-if s3_access_key_id.present? && 1.zero?
-  require 'aws-sdk-s3'
-
-  s3_bucket = ENV[ 'AWS_S3_FEEDS_BUCKET' ]
-  s3_region = ENV[ 'AWS_S3_FEEDS_REGION' ]
-  s3_endpoint = "https://#{s3_bucket}.s3.#{s3_region}.amazonaws.com"
-
-  SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
-    's3_bucket',
-    aws_access_key_id:     s3_access_key_id,
-    aws_secret_access_key: ENV[ 'AWS_S3_FEEDS_SECRET_ACCESS_KEY' ],
-    aws_endpoint:          s3_endpoint,
-    aws_region:            s3_region
-  )
-end
-
-# Set the host name for URL creation
-hostname = ENV[ 'SITE_HOSTNAME'     ].presence || 'localhost:3000'
-protocol = ENV[ 'SITE_URL_PROTOCOL' ].presence || 'https'
-protocol = 'http' if Rails.env.development? || Rails.env.test?
-
-SitemapGenerator::Sitemap.default_host =
-  Rails.application.routes.url_helpers.root_url( host: hostname, protocol: protocol )
-
-SitemapGenerator::Sitemap.create do
-  # Add content from each plugin model that provides it
-  # (there isn't any sitemap-worthy content in main app)
-  ShinyPlugin.models_with_sitemap_items.each do |model|
-    model.sitemap_items.each do |item|
-      add item.url, lastmod: item.content_updated_at, changefreq: item.check_for_updates
-    end
-  end
-end
+ShinySEO::Sitemap.generate
---
+++ plugins/ShinySEO/app/models/shiny_seo/sitemap.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+# ShinySEO plugin for ShinyCMS ~ https://shinycms.org
+#
+# Copyright 2009-2021 Denny de la Haye ~ https://denny.me
+#
+# ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
+
+module ShinySEO
+  # A thin wrapper around the sitemap_generator gem - part of the ShinySEO plugin for ShinyCMS
+  class Sitemap
+    def self.generate
+      SitemapGenerator::Sitemap.default_host = base_url
+
+      use_aws_sdk_adapter if aws_s3_feeds_secret_access_key.present?
+
+      SitemapGenerator::Sitemap.create do
+        ShinyPlugin.models_with_sitemap_items.each do |model|
+          model.sitemap_items.each do |item|
+            add item.url, lastmod: item.content_updated_at, changefreq: item.update_frequency
+          end
+        end
+      end
+    end
+
+    private
+
+    # TODO: resolve duplicate; base_url is in Sitemap and SitemapItem
+    def base_url
+      hostname = ENV[ 'SITE_HOSTNAME'     ].presence || 'localhost:3000'
+      protocol = ENV[ 'SITE_URL_PROTOCOL' ].presence || 'https'
+      protocol = 'http' if Rails.env.development? || Rails.env.test?
+
+      Rails.application.routes.url_helpers.root_url( host: hostname, protocol: protocol )
+    end
+
+    def aws_sdk_config_present?
+      ENV[ 'AWS_S3_FEEDS_ACCESS_KEY_ID' ] && ENV[ 'AWS_S3_FEEDS_SECRET_ACCESS_KEY' ] &&
+        ENV[ 'AWS_S3_FEEDS_BUCKET' ] && ENV[ 'AWS_S3_FEEDS_REGION' ]
+    end
+
+    def use_aws_sdk_adapter
+      require 'aws-sdk-s3'
+
+      SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
+        's3_bucket',
+        aws_secret_access_key: aws_s3_feeds_secret_access_key,
+        aws_access_key_id:     ENV[ 'AWS_S3_FEEDS_ACCESS_KEY_ID' ],
+        aws_region:            ENV[ 'AWS_S3_FEEDS_REGION' ],
+        aws_endpoint:          s3_endpoint
+      )
+    end
+
+    def s3_endpoint
+      s3_bucket = ENV[ 'AWS_S3_FEEDS_BUCKET' ]
+      s3_region = ENV[ 'AWS_S3_FEEDS_REGION' ]
+
+      "https://#{s3_bucket}.s3.#{s3_region}.amazonaws.com"
+    end
+
+    def aws_s3_feeds_secret_access_key
+      ENV[ 'AWS_S3_FEEDS_SECRET_ACCESS_KEY' ].presence
+    end
+  end
+end
