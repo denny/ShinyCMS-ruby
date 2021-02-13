3a4a052fd57a46af04d8d7f42b2071ec39a19181
Denny de la Haye <2021@denny.me>
2021-02-13 20:37:32 +0000

    Namespace controller inheritance

--- app/controllers/admin/email_previews_controller.rb
+++ app/controllers/admin/email_previews_controller.rb
@@ -7,6 +7,6 @@
 # ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
 
 # Base controller for Rails Email Preview, used by /admin/emails
-class Admin::EmailPreviewsController < AdminController
+class Admin::EmailPreviewsController < ShinyCMS::AdminController
   before_action -> { authorize :mailer_preview }, only: %i[ index show ]
 end
