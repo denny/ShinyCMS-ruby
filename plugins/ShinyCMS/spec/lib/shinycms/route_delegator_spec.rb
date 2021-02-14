c6ed86b20482594266f1311bf555a59d5fe20228
Denny de la Haye <2021@denny.me>
2021-02-14 03:06:26 +0000

    Rearrange to suit rubocop

--- plugins/ShinyCMS/spec/lib/shinycms/route_delegator_spec.rb
+++ plugins/ShinyCMS/spec/lib/shinycms/route_delegator_spec.rb
@@ -14,8 +14,8 @@ require 'rails_helper'
 RSpec.describe ShinyCMS::RouteDelegator, type: :helper do
   describe 'method_missing' do
     it 'fails correctly for a method which is missing from main app too' do
-      expect { ::Ckeditor::PicturesController.new.render( inline: '<%= w0rks? %>' ) }
+      expect { Ckeditor::PicturesController.new.render( inline: '<%= w0rks? %>' ) }
         .to raise_error ActionView::Template::Error
-      end
+    end
   end
 end
