12d19967179ea1ce1037bd6c02de0acae4f9fa77
Denny de la Haye <2021@denny.me>
2021-02-17 23:27:55 +0000

    Move dmnd->array conversion to sugar methods on class

--- plugins/ShinyCMS/app/models/concerns/shinycms/plugins_sugar.rb
+++ plugins/ShinyCMS/app/models/concerns/shinycms/plugins_sugar.rb
@@ -19,19 +19,19 @@ module ShinyCMS
       end
 
       def taggable_models
-        models_that_are( :taggable? ).to_a
+        models_that_are( :taggable? )
       end
 
       def votable_models
-        models_that_are( :votable? ).to_a
+        models_that_are( :votable? )
       end
 
       def models_with_demo_data
-        models_that_respond_to( :dump_for_demo? ).to_a
+        models_that_respond_to( :dump_for_demo? )
       end
 
       def models_with_sitemap_items
-        models_that_respond_to( :sitemap_items ).to_a
+        models_that_respond_to( :sitemap_items )
       end
 
       def unshift( plugin )
@@ -68,14 +68,24 @@ module ShinyCMS
         new.with_template( template_path ).to_a
       end
 
+      def taggable_models
+        new.models_that_are( :taggable? ).to_a
+      end
+
+      def votable_models
+        new.models_that_are( :votable? ).to_a
+      end
+
+      def models_with_demo_data
+        new.models_that_respond_to( :dump_for_demo? ).to_a
+      end
+
+      def models_with_sitemap_items
+        new.models_that_respond_to( :sitemap_items ).to_a
+      end
+
       delegate :include?, to: :new
       delegate :loaded?,  to: :new
-
-      delegate :taggable_models, to: :new
-      delegate :votable_models,  to: :new
-
-      delegate :models_with_demo_data,     to: :new
-      delegate :models_with_sitemap_items, to: :new
 
       delegate :models_that_respond_to, to: :new
       delegate :models_that_are,        to: :new
