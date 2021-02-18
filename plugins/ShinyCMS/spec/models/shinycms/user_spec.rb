6f417bdff177692a703d7b251fc272a4b0c6589d
Denny de la Haye <2021@denny.me>
2021-02-18 23:26:49 +0000

    Move concern class methods into class_methods blocks

--- plugins/ShinyCMS/spec/models/shinycms/user_spec.rb
+++ plugins/ShinyCMS/spec/models/shinycms/user_spec.rb
@@ -10,36 +10,6 @@ require 'rails_helper'
 
 # Tests for user model
 RSpec.describe ShinyCMS::User, type: :model do
-  describe 'overridden find_first_by_auth_conditions method' do
-    before do
-      @user = create :user
-    end
-
-    it "finds the user if we submit a username as the 'login' param" do
-      result = described_class.find_first_by_auth_conditions( login: @user.username )
-
-      expect( result.id ).to eq @user.id
-    end
-
-    it "finds the user if we submit an email address as the 'login' param" do
-      result = described_class.find_first_by_auth_conditions( login: @user.email )
-
-      expect( result.id ).to eq @user.id
-    end
-
-    it "finds the user if we submit the 'email' param" do
-      result = described_class.find_first_by_auth_conditions( email: @user.email )
-
-      expect( result.id ).to eq @user.id
-    end
-
-    it "finds the user if we submit the 'username' param" do
-      result = described_class.find_first_by_auth_conditions( username: @user.username )
-
-      expect( result.id ).to eq @user.id
-    end
-  end
-
   describe 'concerns' do
     it_behaves_like ShinyCMS::ShinyEmail do
       let( :addressee ) { create :user }
