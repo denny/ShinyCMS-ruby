970e13d609d99f1238e5a8bf6b47cde18e4d8a5d
Denny de la Haye <2021@denny.me>
2021-02-19 06:04:51 +0000

    Start renaming template concern

--- plugins/ShinyCMS/app/models/concerns/shinycms/shiny_with_template.rb
+++ plugins/ShinyCMS/app/models/concerns/shinycms/shiny_with_template.rb
@@ -8,7 +8,7 @@
 
 module ShinyCMS
   # Common behaviour for templated content - e.g. ShinyPages::Page, ShinyNewsletters::Edition
-  module ShinyWithTemplate
+  module WithTemplate
     extend ActiveSupport::Concern
 
     included do
