f5304a82b061608a2658a02e0306a84754e353ab
Denny de la Haye <2021@denny.me>
2021-02-19 06:04:23 +0000

    Start refactoring lib to controller concern

--- plugins/ShinyCMS/app/lib/shinycms/shiny_sortable.rb
+++ plugins/ShinyCMS/app/lib/shinycms/shiny_sortable.rb
@@ -7,15 +7,21 @@
 # ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
 
 module ShinyCMS
-  # Common methods for handling drag-to-sort content
-  module Sorting
-    def parse_sortable_param( query_string, param_name )
-      CGI.parse( query_string )["#{param_name}[]"]
-    end
+  module Admin
+    # Common methods for handling drag-to-sort content
+    module Sorting
+      extend ActiveSupport::Concern
 
-    def apply_sort_order( collection, sort_order )
-      sort_order.each_with_index do |id, index|
-        collection.find( id ).update!( position: index + 1 )
+      included do
+        def parse_sortable_param( query_string, param_name )
+          CGI.parse( query_string )["#{param_name}[]"]
+        end
+
+        def apply_sort_order( collection, sort_order )
+          sort_order.each_with_index do |id, index|
+            collection.find( id ).update!( position: index + 1 )
+          end
+        end
       end
     end
   end
