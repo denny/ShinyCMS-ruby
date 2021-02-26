57d568f95bca51702445c93ab15adad79199eac2
Denny de la Haye <2021@denny.me>
2021-02-26 18:49:28 +0000

    Add best effort fallback to translated_name, and rename it

--- plugins/ShinyCMS/app/models/concerns/shinycms/shiny_class_name.rb
+++ plugins/ShinyCMS/app/models/concerns/shinycms/shiny_class_name.rb
@@ -7,13 +7,18 @@
 # ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
 
 module ShinyCMS
-  # Methods to facilitate easy translation of model class names into human-readable versions
-  module ShinyClassName
+  # Methods to turn model class names into human-readable names - e.g. 'blog post'
+  # Uses the locale string if there is one, otherwise makes best effort
+  module HasReadableName
     extend ActiveSupport::Concern
 
     class_methods do
-      def translated_name
-        I18n.t( "shinycms.models.names.#{i18n_label}" )
+      def readable_name
+        if I18n.exists? "shinycms.models.names.#{i18n_label}"
+          I18n.t "shinycms.models.names.#{i18n_label}"
+        else
+          name.demodulize.humanize.downcase
+        end
       end
 
       def i18n_label
