64332e2698b1e1c20907fd1865159219ad94511e
Denny de la Haye <2021@denny.me>
2021-03-03 23:55:32 +0000

    Improve authorization failure flows for profile editing

--- plugins/ShinyProfiles/spec/requests/shiny_profiles/profiles_controller_spec.rb
+++ plugins/ShinyProfiles/spec/requests/shiny_profiles/profiles_controller_spec.rb
@@ -58,20 +58,20 @@ RSpec.describe ShinyProfiles::ProfilesController, type: :request do
       expect( response.body ).to have_title user.profile.name
     end
 
-    it "redirects if the profile doesn't belong to the logged-in user" do
-      create :top_level_page
+    context 'when the username in the URL is not the logged-in user' do
+      it 'redirects to the view profile page for that username' do
+        user1 = create :user
+        user2 = create :user
+        sign_in user2
 
-      user1 = create :user
-      user2 = create :user
-      sign_in user2
+        get shiny_profiles.edit_profile_path( user1.username )
 
-      get shiny_profiles.edit_profile_path( user1.username )
-
-      expect( response      ).to have_http_status :found
-      expect( response      ).to redirect_to main_app.root_path
-      follow_redirect!
-      expect( response      ).to have_http_status :ok
-      expect( response.body ).to have_css '.alerts', text: I18n.t( 'shiny_profiles.profiles.edit.not_authorized' )
+        expect( response      ).to have_http_status :found
+        expect( response      ).to redirect_to shiny_profiles.profile_path( user1.username )
+        follow_redirect!
+        expect( response      ).to have_http_status :ok
+        expect( response.body ).to have_title user1.profile.name
+      end
     end
   end
 
@@ -177,26 +177,26 @@ RSpec.describe ShinyProfiles::ProfilesController, type: :request do
       expect( response.body ).to have_field 'profile[public_name]', with: new_name
     end
 
-    it "redirects if the profile doesn't belong to the logged-in user" do
-      create :top_level_page
+    context "when a logged-in user attempts to update somebody else's profile" do
+      it 'they get redirected to their own profile edit page' do
+        user1 = create :user
+        user2 = create :user
+        sign_in user2
 
-      user1 = create :user
-      user2 = create :user
-      sign_in user2
+        new_name = Faker::Books::CultureSeries.unique.culture_ship
 
-      new_name = Faker::Books::CultureSeries.unique.culture_ship
-
-      put shiny_profiles.profile_path( user1.username ), params: {
-        profile: {
-          public_name: new_name
+        put shiny_profiles.profile_path( user1.username ), params: {
+          profile: {
+            public_name: new_name
+          }
         }
-      }
 
-      expect( response      ).to have_http_status :found
-      expect( response      ).to redirect_to main_app.root_path
-      follow_redirect!
-      expect( response      ).to have_http_status :ok
-      expect( response.body ).to have_css '.alerts', text: I18n.t( 'shiny_profiles.profiles.update.not_authorized' )
+        expect( response      ).to have_http_status :found
+        expect( response      ).to redirect_to shiny_profiles.edit_profile_path( user2.username )
+        follow_redirect!
+        expect( response      ).to have_http_status :ok
+        expect( response.body ).to have_title user2.profile.name
+      end
     end
 
     it 'adds profile links' do
