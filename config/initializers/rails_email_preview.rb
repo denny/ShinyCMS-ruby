338bac3f98eabd9d160caa6256a718f14bd15a93
Denny de la Haye <2021@denny.me>
2021-03-05 19:31:46 +0000

    Couldn't get it to work without this bodge, annoyingly

---
+++ config/initializers/rails_email_preview.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# ShinyCMS ~ https://shinycms.org
+#
+# Copyright 2009-2021 Denny de la Haye ~ https://denny.me
+#
+# ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
+
+# TODO: FIXME: Currently REP's routing stops working if this file is removed.
+# I'm assuming it's something to do with load order.
+
+require_relative '../../plugins/ShinyCMS/config/initializers/rails_email_preview'
