4bf44e2341a87e2ff28244bfe2c542dc6b5b0c93
Denny de la Haye <2021@denny.me>
2021-03-05 18:56:37 +0000

    Start moving REP into the core plugin

--- plugins/ShinyCMS/app/controllers/shinycms/admin/email_previews_controller.rb
+++ plugins/ShinyCMS/app/controllers/shinycms/admin/email_previews_controller.rb
@@ -6,7 +6,9 @@
 #
 # ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
 
-# Base controller for Rails Email Preview, used by /admin/emails
-class Admin::EmailPreviewsController < ShinyCMS::AdminController
-  before_action -> { authorize :mailer_preview }, only: %i[ index show ]
+module ShinyCMS
+  # Base controller for Rails Email Preview
+  class Admin::EmailPreviewsController < ShinyCMS::AdminController
+    before_action -> { authorize :mailer_preview }, only: %i[ index show ]
+  end
 end
