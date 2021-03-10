18430da717397fc2f4891f15a790ac54bb029821
Denny de la Haye <2021@denny.me>
2021-03-10 13:37:09 +0000

    Check and enforce do-not-contact before doing anything else

--- plugins/ShinyCMS/app/mailers/shinycms/user_mailer.rb
+++ plugins/ShinyCMS/app/mailers/shinycms/user_mailer.rb
@@ -18,61 +18,72 @@ module ShinyCMS
     track click: false
 
     def confirmation_instructions( user, token, _args = nil )
-      @resource = @user = user
-      @token = token
+      return if not_ok_to_email? user
 
-      return if DoNotContact.list_includes? @user.email # TODO: make this happen without explicit call
+      set_instance_vars( user, token )
 
-      mail to: user.email_to, subject: subject_for( :confirmation ) do |format|
-        format.html
-        format.text
-      end
-    end
-
-    def reset_password_instructions( user, token, _args = nil )
-      @resource = @user = user
-      @token = token
-
-      return if DoNotContact.list_includes? @user.email # TODO: make this happen without explicit call
-
-      mail to: user.email_to, subject: subject_for( :reset_password ) do |format|
-        format.html
-        format.text
-      end
-    end
-
-    def password_changed_instructions( user, _args = nil )
-      @resource = @user = user
-
-      return if DoNotContact.list_includes? @user.email # TODO: make this happen without explicit call
-
-      mail to: user.email_to, subject: subject_for( :password_changed ) do |format|
+      mail to: @user.email_to, subject: subject_for( :confirmation ) do |format|
         format.html
         format.text
       end
     end
 
     def email_changed_instructions( user, _args = nil )
-      @resource = @user = user
+      return if not_ok_to_email? user
 
-      return if DoNotContact.list_includes? @user.email # TODO: make this happen without explicit call
+      set_instance_vars( user )
 
-      mail to: user.email_to, subject: subject_for( :email_changed ) do |format|
+      mail to: @user.email_to, subject: subject_for( :email_changed ) do |format|
+        format.html
+        format.text
+      end
+    end
+
+    def password_changed_instructions( user, _args = nil )
+      return if not_ok_to_email? user
+
+      set_instance_vars( user )
+
+      mail to: @user.email_to, subject: subject_for( :password_changed ) do |format|
+        format.html
+        format.text
+      end
+    end
+
+    def reset_password_instructions( user, token, _args = nil )
+      return if not_ok_to_email? user
+
+      set_instance_vars( user, token )
+
+      mail to: @user.email_to, subject: subject_for( :reset_password ) do |format|
         format.html
         format.text
       end
     end
 
     def unlock_instructions( user, token, _args = nil )
-      @resource = @user = user
-      @token = token
+      return if not_ok_to_email? user
 
-      return if DoNotContact.list_includes? @user.email # TODO: make this happen without explicit call
+      set_instance_vars( user, token )
 
-      mail to: user.email_to, subject: subject_for( :unlock ) do |format|
+      mail to: @user.email_to, subject: subject_for( :unlock ) do |format|
         format.html
         format.text
       end
+    end
+
+    private
+
+    def not_ok_to_email?( user )
+      DoNotContact.list_includes? user.email
+    end
+
+    def set_instance_vars( user, token = nil )
+      @resource = @user = user
+
+      @token = token if token
+
+      true
     end
 
     def subject_for( mailer_name )
