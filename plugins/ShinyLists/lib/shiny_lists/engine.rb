1c1faf7ccb68363935cef0dcccbc95c46f8b2120
Denny de la Haye <2021@denny.me>
2021-03-14 12:01:48 +0000

    Testing gem in Not Defined In Production shocker

--- plugins/ShinyLists/lib/shiny_lists/engine.rb
+++ plugins/ShinyLists/lib/shiny_lists/engine.rb
@@ -21,7 +21,7 @@ module ShinyLists
 
     initializer 'shiny_lists.factories', after: 'factory_bot.set_factory_paths' do
       plugin_factories = File.expand_path( '../../spec/factories', __dir__ )
-      FactoryBot.definition_file_paths << plugin_factories
+      FactoryBot.definition_file_paths << plugin_factories if defined? FactoryBot
     end
   end
 end
