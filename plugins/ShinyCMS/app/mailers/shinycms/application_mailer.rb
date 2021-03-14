d7c24a8fd56b199c9af9104b56b9ed4603084e38
Denny de la Haye <2021@denny.me>
2021-03-14 07:07:54 +0000

    Finally worked out how to stop processing early in mailers, yay

--- plugins/ShinyCMS/app/mailers/shinycms/application_mailer.rb
+++ plugins/ShinyCMS/app/mailers/shinycms/application_mailer.rb
@@ -6,8 +6,9 @@
 #
 # ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
 
-# Mailers that inherit from here must implement `check_feature_flags` (probably using `enforce_feature_flags`)
-# and `check_ok_to_email` (probably using either `enforce_ok_to_email` or `enforce_do_not_contact`).
+# Mailers that inherit from here must implement `check_feature_flags` (probably
+# using `enforce_feature_flags`) and `check_ok_to_email` (probably using either
+# `enforce_ok_to_email` or `enforce_do_not_contact`)
 
 module ShinyCMS
   # Base class for mailers - part of the ShinyCMS core plugin
@@ -17,6 +18,8 @@ module ShinyCMS
     helper SiteNameHelper
 
     before_action :check_feature_flags
+    before_action :add_theme_view_path
+
     after_action :check_ok_to_email
 
     default from: -> { default_email }
@@ -27,31 +30,36 @@ module ShinyCMS
 
     private
 
-    def enforce_feature_flags( *feature_flag_names )
-      mail.perform_deliveries = false unless feature_flags_enabled?( feature_flag_names )
+    def stop!
+      mail.perform_deliveries = false
+      # Stop processing, now. See https://is.gd/ActionMailerStop (and last line of https://is.gd/ActionMailerCallbacks)
+      self.response_body = nil
     end
 
-    def enforce_ok_to_email( recipient )
-      # .ok_to_email? checks .confirmed status (AKA double opt-in) and DoNotContact status
-      mail.perform_deliveries = false unless recipient&.ok_to_email?
+    def enforce_feature_flags( *feature_flag_names )
+      stop! unless feature_flags_enabled? feature_flag_names
+    end
+
+    def enforce_ok_to_email( email_recipient )
+      stop! if email_recipient.blank?
+      # HasEmail.not_ok_to_email? checks on .confirmed status
+      # (AKA double opt-in) and DoNotContact status
+      stop! if email_recipient.not_ok_to_email?
     end
 
     def enforce_do_not_contact( email_address )
-      # For the rare cases when you don't want to check .confirmed status (e.g. confirmation emails)
-      mail.perform_deliveries = false if DoNotContact.list_includes? email_address
+      # You should use `enforce_ok_to_email` rather than this method whenever possible.
+      # The only exception is for mailer actions where checking the .confirmed status
+      # is impossible or would make no sense (e.g. when sending the confirmation email!)
+      stop! if DoNotContact.list_includes? email_address
     end
 
     def feature_flags_enabled?( *feature_flag_names )
       ( [ :send_emails ] + [ feature_flag_names ] ).flatten.all? { |name| FeatureFlag.enabled? name }
     end
 
-    def add_to_view_paths( plugin_path )
-      # Add plugin view path, if given
-      # TODO: Move into ShinyCMS::Plugin ?
-      prepend_view_path plugin_path
-
-      # Apply the configured theme, if any, by adding it above the rest
-      # TODO: Move into ShinyCMS::Theme ?
+    def add_theme_view_path
+      # Apply the configured theme, if any, by adding it at top of view paths
       theme = ShinyCMS::Theme.get
       prepend_view_path theme.view_path if theme.present?
     end
