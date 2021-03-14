0dd6418771436227c187bfbf251e07856bb6eaed
Denny de la Haye <2021@denny.me>
2021-03-14 10:14:08 +0000

    Other engines seem to load their gems in here

--- plugins/ShinySearch/lib/shiny_search.rb
+++ plugins/ShinySearch/lib/shiny_search.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
+# ShinySearch plugin for ShinyCMS ~ https://shinycms.org
+#
+# Copyright 2009-2021 Denny de la Haye ~ https://denny.me
+#
+# ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
+
 require 'shiny_search/engine'
+
+require 'algoliasearch-rails'
+require 'pg_search'
 
 # Namespace wrapper
 module ShinySearch
