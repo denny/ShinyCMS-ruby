84809b55bc215c8261ff1343d678687f60e74c8e
Denny de la Haye <2021@denny.me>
2021-03-24 00:26:45 +0000

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # less error prone than running all of your migrations from scratch. Old migrations may fail
 # to apply correctly if those migrations use external dependencies or application code.
 
-ActiveRecord::Schema.define(version: 2021_02_26_060425) do
+ActiveRecord::Schema.define(version: 2021_03_20_143654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -155,6 +155,20 @@ ActiveRecord::Schema.define(version: 2021_02_26_060425) do
     t.datetime "created_at", precision: 0, null: false
     t.datetime "updated_at", precision: 0, null: false
     t.index ["creator_id"], name: "index_blazer_queries_on_creator_id"
+  end
+
+  create_table "flipper_features", force: :cascade do |t|
+    t.text "key", null: false
+    t.datetime "created_at", precision: 0, null: false
+    t.datetime "updated_at", precision: 0, null: false
+  end
+
+  create_table "flipper_gates", force: :cascade do |t|
+    t.text "feature_key", null: false
+    t.text "key", null: false
+    t.text "value"
+    t.datetime "created_at", precision: 0, null: false
+    t.datetime "updated_at", precision: 0, null: false
   end
 
   create_table "pg_search_documents", force: :cascade do |t|
