778bca23981ce1e5b391ea89234f4a852c6efd8b
Denny de la Haye <2021@denny.me>
2021-04-09 11:55:45 +0100

    Add gem for Sentry.io monitoring service

--- plugins/ShinyCMS/lib/shinycms.rb
+++ plugins/ShinyCMS/lib/shinycms.rb
@@ -69,8 +69,10 @@ end
 require 'cloudflare/rails'
 
 # Monitoring services
-require 'airbrake' if ENV[ 'AIRBRAKE_API_KEY' ].present?
-require 'bugsnag'  if ENV[ 'BUGSNAG_API_KEY'  ].present?
+require 'airbrake'     if ENV[ 'AIRBRAKE_API_KEY' ].present?
+require 'bugsnag'      if ENV[ 'BUGSNAG_API_KEY'  ].present?
+require 'sentry-ruby'  if ENV[ 'SENTRY_DSN'       ].present?
+require 'sentry-rails' if ENV[ 'SENTRY_DSN'       ].present?
 
 # Top-level namespace wrapper
 module ShinyCMS
