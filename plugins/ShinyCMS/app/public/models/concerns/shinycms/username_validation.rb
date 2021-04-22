ab8a5956aa4ab310d23f7dd04823ec189a2e4b2e
Denny de la Haye <2021@denny.me>
2021-04-23 00:46:35 +0100

    Move username regex into concern

---
+++ plugins/ShinyCMS/app/public/models/concerns/shinycms/username_validation.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# ShinyCMS ~ https://shinycms.org
+#
+# Copyright 2009-2021 Denny de la Haye ~ https://denny.me
+#
+# ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
+
+module ShinyCMS
+  # Regex for validating usernames
+  module UsernameValidation
+    # Allowed characters for usernames: a-z A-Z 0-9 . _ -
+    REGEX = %r{[a-zA-Z0-9][-_.a-zA-Z0-9]*}
+    public_constant :REGEX
+
+    ANCHORED_REGEX = %r{\A#{REGEX}\z}
+    public_constant :ANCHORED_REGEX
+  end
+end
