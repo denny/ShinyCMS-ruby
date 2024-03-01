c445d4dd023f8e07a404afdb026d416277a0726f
Denny <2023@denny.me>
2024-03-01 21:06:30 +0000

    Fix minor rubocop complaints

--- plugins/ShinyCMS/spec/models/shinycms/setting_spec.rb
+++ plugins/ShinyCMS/spec/models/shinycms/setting_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe ShinyCMS::Setting, type: :model do
       setting.level = 'user'
 
       expect { _rbp = setting.save( validate: false ) }
-        .to raise_error ShinyCMS::Setting::CannotUpdateLockedSetting, 'Attempted to update a locked setting'
+        .to raise_error described_class::CannotUpdateLockedSetting, 'Attempted to update a locked setting'
     end
   end
 
