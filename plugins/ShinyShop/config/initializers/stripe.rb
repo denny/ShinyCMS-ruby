2c1c4e7ffbdf96805cd0a81e0cf04a27a74941fe
Denny <2023@denny.me>
2024-06-03 14:58:14 +0100

    Pass customer details into checkout mailer

--- plugins/ShinyShop/config/initializers/stripe.rb
+++ plugins/ShinyShop/config/initializers/stripe.rb
@@ -15,20 +15,21 @@ Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 
 StripeEvent.signing_secret = ENV['STRIPE_SIGNING_SECRET']
 StripeEvent.configure do |events|
-    # Define subscriber behavior based on the event object
-    # event.class       # => Stripe::Event
-    # event.type        # => "charge.failed"
-    # event.data.object # => #<Stripe::Charge:0x3fcb34c115f8>
+  # Define subscriber behavior based on the event object
+  # event.class       # => Stripe::Event
+  # event.type        # => "charge.failed"
+  # event.data.object # => #<Stripe::Charge:0x3fcb34c115f8>
 
-    events.subscribe 'checkout.session.completed' do |event|
-        Rails.logger.info 'StripeEvent received: checkout.session.completed'
+  events.subscribe 'checkout.session.completed' do |event|
+    Rails.logger.info 'StripeEvent received: checkout.session.completed'
 
-        # Send confirmation email to customer
-        ShopMailer.confirmation( event ).deliver_later
-    end
+    # Send confirmation email to customer
+    customer = event.data.object.customer_details
+    ShinyShop::ShopMailer.confirmation( customer.email ).deliver_later
+  end
 
-    events.all do |event|
-        # Handle all event types - logging, etc.
-        Rails.logger.info "StripeEvent received: #{ event.type }"
-    end
+  events.all do |event|
+    # Handle all event types - logging, etc.
+    Rails.logger.info "StripeEvent received: #{event.type}"
+  end
 end
