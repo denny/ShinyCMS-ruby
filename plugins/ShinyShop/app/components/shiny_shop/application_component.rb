f86147c54d66ac0433057fedd598408c7056ef7b
Denny <2023@denny.me>
2024-09-11 15:07:13 +0100

    Add admin menu for shop products

---
+++ plugins/ShinyShop/app/components/shiny_shop/application_component.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# ShinyShop plugin for ShinyCMS ~ https://shinycms.org
+#
+# Copyright 2009-2024 Denny de la Haye ~ https://denny.me
+#
+# ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
+
+module ShinyShop
+  # Base class for view components - part of the ShinyShop plugin for ShinyCMS
+  class ApplicationComponent < ViewComponent::Base
+    include ShinyCMS::ViewComponentBase
+  end
+end
