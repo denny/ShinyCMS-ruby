4dc3123045b011614c3cb67eefe06202befce82b
Denny <2025@denny.me>
2025-03-06 18:59:20 +0000

    Specify self.abstract_class in app base model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -6,4 +6,6 @@
 #
 # ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
 
-class ApplicationRecord < ActiveRecord::Base; end
+class ApplicationRecord < ActiveRecord::Base
+  self.abstract_class = true
+end
