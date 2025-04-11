0e10eb32384fcf7df019bf653268d118d25ae0f1
Denny <2025@denny.me>
2025-04-11 18:25:58 +0100

    Add some more common PHP URLs from my logging

--- plugins/ShinyCMS/config/initializers/rack_attack.rb
+++ plugins/ShinyCMS/config/initializers/rack_attack.rb
@@ -13,7 +13,8 @@ Rack::Attack.blocklisted_responder =
     [ 204, {}, [ 'No Content' ] ]
   end
 
-Rack::Attack.blocklist( 'block all access to common Wordpress admin URLs' ) do |request|
-  # Requests are blocked if the return value is truthy
-  request.path.start_with?( '/wp-admin', '/wp-login' )
+  Rack::Attack.blocklist( 'block common Wordpress and other PHP URLs' ) do |request|
+    # Requests are blocked if the return value is truthy
+    request.path.start_with?( '/wp-admin', '/wp-login', '/wp-content', '/account/password/new' )
+  end
 end
