b05465c95c08c0addf3b0791b067a24df3203a1b
Denny <2025@denny.me>
2025-04-24 15:18:17 +0100

    Minor whitespace tweaks

--- plugins/ShinyCMS/config/initializers/pagy.rb
+++ plugins/ShinyCMS/config/initializers/pagy.rb
@@ -15,8 +15,8 @@
 # You can set any pagy variable as a Pagy::DEFAULT. They can also be overridden per instance by just passing them to
 # Pagy.new|Pagy::Countless.new|Pagy::Calendar::*.new or any of the #pagy* controller methods
 # Here are the few that make more sense as DEFAULTs:
-# Pagy::DEFAULT[:limit]  = 20                # default
-Pagy::DEFAULT[:size] = 10                    # default
+# Pagy::DEFAULT[:limit] = 20                 # default
+Pagy::DEFAULT[:size] = 10
 # Pagy::DEFAULT[ :size ] = [ 3, 3, 3, 3 ]
 # Pagy::DEFAULT[:ends] = true                # default
 
