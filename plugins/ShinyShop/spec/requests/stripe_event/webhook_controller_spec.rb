e3858ad5296435b2d4b10144835c350006c716e5
Denny <2025@denny.me>
2025-05-08 12:48:09 +0100

    Fix linting complaints

--- plugins/ShinyShop/spec/requests/stripe_event/webhook_controller_spec.rb
+++ plugins/ShinyShop/spec/requests/stripe_event/webhook_controller_spec.rb
@@ -10,21 +10,19 @@ require 'rails_helper'
 
 # Tests for stripe
 RSpec.describe StripeEvent::WebhookController, type: :request do
-  context '' do
-    describe '' do
-      it '' do
-        post '/shop/stripe_events', params: {
-          "object": {
-            "id": "cs_test_a1EpQUx46ZmXVRrlDCpDTyy7DNsXwDmc3IRH12Bf8KDAm0h35Aeh18IDlG",
-            "object": "checkout.session",
-          }
+  describe 'with Stripe::Event' do
+    it 'creates an event' do
+      post '/shop/stripe_events', params: {
+        object: {
+          id:     'cs_test_a1EpQUx46ZmXVRrlDCpDTyy7DNsXwDmc3IRH12Bf8KDAm0h35Aeh18IDlG',
+          object: 'checkout.session'
         }
+      }
 
-        # binding.pry
+      # binding.pry
 
-        expect( response      ).to have_http_status :ok
-        expect( response.body ).to include 'No products found'
-      end
+      expect( response      ).to have_http_status :ok
+      expect( response.body ).to include 'No products found'
     end
   end
 end
