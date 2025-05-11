6034ae580bc6b47e5a63245fff4955964b57d198
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2025-05-11 14:10:28 +0100

    Fixed the 11º offense

--- plugins/ShinyLists/spec/models/shiny_lists/subscription_spec.rb
+++ plugins/ShinyLists/spec/models/shiny_lists/subscription_spec.rb
@@ -9,36 +9,34 @@
 require 'rails_helper'
 
 # Tests for mailing list subscriptions model
-module ShinyLists
-  RSpec.describe Subscription, type: :model do
-    describe 'scopes' do
-      let( :list ) { create :mailing_list }
+RSpec.describe ShinyLists::Subscription, type: :model do
+  describe 'scopes' do
+    let( :list ) { create :mailing_list }
 
-      describe '.active' do
-        it 'does not include subscribers who have unsubscribed' do
-          create :mailing_list_subscription, list: list
-          create :mailing_list_subscription, list: list, unsubscribed_at: 1.day.ago
+    describe '.active' do
+      it 'does not include subscribers who have unsubscribed' do
+        create :mailing_list_subscription, list: list
+        create :mailing_list_subscription, list: list, unsubscribed_at: 1.day.ago
 
-          expect( list.subscriptions.active.count ).to eq 1
-        end
+        expect( list.subscriptions.active.count ).to eq 1
       end
     end
+  end
 
-    describe 'instance methods' do
-      describe '.unsubscribe' do
-        it 'sets the unsubscribed timestamp' do
-          sub1 = create :mailing_list_subscription
-          expect( sub1.reload.unsubscribed_at ).to be_nil
-          sub1.unsubscribe
-          expect( sub1.reload.unsubscribed_at ).not_to be_nil
-        end
+  describe 'instance methods' do
+    describe '.unsubscribe' do
+      it 'sets the unsubscribed timestamp' do
+        sub1 = create :mailing_list_subscription
+        expect( sub1.reload.unsubscribed_at ).to be_nil
+        sub1.unsubscribe
+        expect( sub1.reload.unsubscribed_at ).not_to be_nil
       end
     end
+  end
 
-    describe 'concerns' do
-      it_behaves_like ShinyCMS::ProvidesDemoSiteData do
-        let( :model ) { described_class }
-      end
+  describe 'concerns' do
+    it_behaves_like ShinyCMS::ProvidesDemoSiteData do
+      let( :model ) { described_class }
     end
   end
 end
