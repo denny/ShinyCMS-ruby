b46ac0631217dab6ae7f5fb285cb4c070e7ac730
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2025-05-11 14:04:28 +0100

    Fixed the 8º offense

--- plugins/ShinyInserts/spec/models/shiny_inserts/element_spec.rb
+++ plugins/ShinyInserts/spec/models/shiny_inserts/element_spec.rb
@@ -9,16 +9,14 @@
 require 'rails_helper'
 
 # Tests for insert elements
-module ShinyInserts
-  RSpec.describe Element, type: :model do
-    describe 'concerns' do
-      it_behaves_like ShinyCMS::Element do
-        let( :element ) { create :insert_element }
-      end
+RSpec.describe ShinyInserts::Element, type: :model do
+  describe 'concerns' do
+    it_behaves_like ShinyCMS::Element do
+      let( :element ) { create :insert_element }
+    end
 
-      it_behaves_like ShinyCMS::ProvidesDemoSiteData do
-        let( :model ) { described_class }
-      end
+    it_behaves_like ShinyCMS::ProvidesDemoSiteData do
+      let( :model ) { described_class }
     end
   end
 end
