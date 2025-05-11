eaad58068308414fd55d46cfc6bd3ac00fd13d3f
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2025-05-11 14:30:26 +0100

    Fixed the 25º offense

--- plugins/ShinyProfiles/spec/models/shiny_profiles/profile_spec.rb
+++ plugins/ShinyProfiles/spec/models/shiny_profiles/profile_spec.rb
@@ -9,30 +9,28 @@
 require 'rails_helper'
 
 # Tests for user profile model
-module ShinyProfiles
-  RSpec.describe Profile, type: :model do
-    describe 'Instance methods' do
-      describe '.name' do
-        it 'returns the public_name if one is set' do
-          user = create :user
-          user.profile.update!( public_name: 'Testy McTestface' )
+RSpec.describe ShinyProfiles::Profile, type: :model do
+  describe 'Instance methods' do
+    describe '.name' do
+      it 'returns the public_name if one is set' do
+        user = create :user
+        user.profile.update!( public_name: 'Testy McTestface' )
 
-          expect( user.profile.name ).to eq user.profile.public_name
-        end
+        expect( user.profile.name ).to eq user.profile.public_name
+      end
 
-        it "returns the user's username if no public_name is set" do
-          user = create :user
-          user.profile.update!( public_name: nil )
+      it "returns the user's username if no public_name is set" do
+        user = create :user
+        user.profile.update!( public_name: nil )
 
-          expect( user.profile.name ).to eq user.username
-        end
+        expect( user.profile.name ).to eq user.username
       end
     end
+  end
 
-    describe 'concerns' do
-      it_behaves_like ShinyCMS::ProvidesDemoSiteData do
-        let( :model ) { described_class }
-      end
+  describe 'concerns' do
+    it_behaves_like ShinyCMS::ProvidesDemoSiteData do
+      let( :model ) { described_class }
     end
   end
 end
