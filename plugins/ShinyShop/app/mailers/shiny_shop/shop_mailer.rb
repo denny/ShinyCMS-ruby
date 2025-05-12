fa36f783155bee4587c98750c844854fd4446dc6
Denny <2025@denny.me>
2025-05-12 17:52:28 +0100

    Revert last change

--- plugins/ShinyShop/app/mailers/shiny_shop/shop_mailer.rb
+++ plugins/ShinyShop/app/mailers/shiny_shop/shop_mailer.rb
@@ -6,24 +6,26 @@
 #
 # ShinyCMS is free software; you can redistribute it and/or modify it under the terms of the GPL (version 2 or later)
 
-# Mailer to send emails from shop features - part of the ShinyShop plugin for ShinyCMS
-class ShinyShop::ShopMailer < ApplicationMailer
-  layout 'shiny_shop/layouts/shop_mailer'
+module ShinyShop
+  # Mailer to send emails from shop features - part of the ShinyShop plugin for ShinyCMS
+  class ShopMailer < ApplicationMailer
+    layout 'shiny_shop/layouts/shop_mailer'
 
-  def confirmation( customer_email )
-    mail to: customer_email, subject: I18n.t( 'shiny_shop.mailers.shop_mailer.confirmation.subject' ) do |format|
-      format.html
-      format.text
+    def confirmation( customer_email )
+      mail to: customer_email, subject: I18n.t( 'shiny_shop.mailers.shop_mailer.confirmation.subject' ) do |format|
+        format.html
+        format.text
+      end
     end
-  end
 
-  private
+    private
 
-  def check_feature_flags
-    enforce_feature_flags :shop
-  end
+    def check_feature_flags
+      enforce_feature_flags :shop
+    end
 
-  def check_ok_to_email
-    enforce_ok_to_email @recipient_email
+    def check_ok_to_email
+      enforce_ok_to_email @recipient_email
+    end
   end
 end
