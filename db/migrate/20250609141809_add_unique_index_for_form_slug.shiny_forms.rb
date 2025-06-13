f7f7028c3afbb9f659e1225e5fff72be7b36b245
Denny <2025@denny.me>
2025-06-13 14:36:00 +0100

    Database changes for rubocop index complaints

---
+++ db/migrate/20250609141809_add_unique_index_for_form_slug.shiny_forms.rb
@@ -0,0 +1,7 @@
+# This migration comes from shiny_forms (originally 20250609135010)
+class AddUniqueIndexForFormSlug < ActiveRecord::Migration[8.0]
+  def change
+    remove_index :shiny_forms_forms, :slug
+    add_index :shiny_forms_forms, :slug, unique: true
+  end
+end
